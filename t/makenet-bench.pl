40669dcc61d66d4b84bdcdd42cc0f5205c5894b1
eserte <eserte>
2003-10-08 11:47:20 +0000

    options results

--- t/makenet-bench.pl
+++ t/makenet-bench.pl
@@ -21,7 +21,21 @@ use lib ("$FindBin::RealBin/..",
 	);
 use Strassen;
 use strict;
-#use BBBikeXS;
+use Getopt::Long;
+
+my $usexs = 0;
+my $usecache = 0;
+
+if (!GetOptions("xs!" => \$usexs,
+		"cache!" => \$usecache,
+	       )) {
+    die "usage: $0 [-[no]xs] [-[no]cache] iter";
+}
+
+if ($usexs) {
+    eval 'use BBBikeXS'; die $@ if $@;
+}
+
 my $s = MultiStrassen->new("strassen", "landstrassen", "landstrassen2");
 my $iter = shift || 10;
 
@@ -29,7 +43,25 @@ timethese($iter,
 	  {
 	   makenet => sub {
 	       my $net = StrassenNetz->new($s);
-	       $net->make_net_classic(UseCache => 0);
+	       $net->make_net_classic(UseCache => $usecache);
 	   },
 	  });
 __END__
+
+Results under RedHat 8.0, Intel Pentium 4 2.4MHz, perl 5.8.0
+
+With BBBikeXS, without cache:
+Benchmark: timing 10 iterations of makenet...
+   makenet:  6 wallclock secs ( 6.29 usr +  0.08 sys =  6.37 CPU) @  1.57/s (n=10)
+
+Without BBBikeXS, without cache:
+Benchmark: timing 10 iterations of makenet...
+   makenet: 12 wallclock secs (11.29 usr +  0.07 sys = 11.36 CPU) @  0.88/s (n=10)
+
+With BBBikeXS, with cache (I think UseCache is ignored in this case):
+Benchmark: timing 10 iterations of makenet...
+   makenet:  6 wallclock secs ( 6.29 usr +  0.05 sys =  6.34 CPU) @  1.58/s (n=10)
+
+Without BBBikeXS, with cache:
+Benchmark: timing 10 iterations of makenet...
+   makenet:  6 wallclock secs ( 5.83 usr +  0.11 sys =  5.94 CPU) @  1.68/s (n=10)
