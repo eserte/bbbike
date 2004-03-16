2cacb0cfee2a7ebe5f73e1f16c75350b2f22bfbf
eserte <eserte>
2004-03-16 23:22:06 +0000

    Implementation of -onefile

--- Strassen/MapInfo.pm
+++ Strassen/MapInfo.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: MapInfo.pm,v 1.10 2004/03/15 20:15:15 eserte Exp $
+# $Id: MapInfo.pm,v 1.11 2004/03/16 23:22:06 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (c) 2004 Slaven Rezic. All rights reserved.
@@ -335,6 +335,7 @@ sub create_mif_mid_from_data_directory {
     my $version = "300";
 
     require Template;
+    require Storable;
 
     my $conv;
     my $trim_accuracy;
@@ -359,98 +360,295 @@ sub create_mif_mid_from_data_directory {
 	$coordsysline = qq{COORDSYS NonEarth Units "m" Bounds ([% minx %],[% miny %]) ([% maxx %],[% maxy %])\n};
     }
 
-    my @column_names = qw(Name Category
-			  Elevation
-			  Trafficlight_Comment Trafficlight_Category
-			  Blocking_Comment Blocking_Category
-			  Cyclepath_Comment Cyclepath_Category
-			  Quality_Comment Quality_Category
-			  Handicap_Comment Handicap_Category
-			  Mount_Comment Mount_Category
-			  Path_Comment Path_Category
-			  Route_Comment Route_Category
-			  Tram_Comment Tram_Category
-			  Traffic_Comment Traffic_Category
-			  Ferry_Comment Ferry_Category
-			  Brunnel_Comment Brunnel_Category
-			  Green_Comment Green_Category
-			  Nolighting_Comment Nolighting_Category
-			  Comment Comment_Category
-			 );
-    # XXX missing columns: Author, AcquireAuthor,CreationDate, AcquireDate
-    my %column_to_index;
-    {
-	my $i = 0;
-	for (@column_names) {
-	    $column_to_index{$_} = $i;
-	    $i++;
-	}
+    my $scope = $args{scope}; # undef, "city" or "region"
+    if (defined $scope) {
+	$scope = "region" if $scope eq "brb";
+	die "Wrong scope $scope" if $scope !~ /^( city | region )$/x;
     }
+    my $if_city = sub {
+	!defined $scope || $scope eq 'city' ? @_ : ();
+    };
+    my $if_region = sub {
+	!defined $scope || $scope eq 'region' ? @_ : ();
+    };
+
+    my @street_files =
+	([[$if_city->("strassen", "plaetze"),
+	   $if_region->("landstrassen", "landstrassen2"), "faehren"],
+	  "strassen", NAME => "Name", CAT => "Category"],
+	 ["ampeln", "ampeln",
+	  NAME => "Trafficlight_Comment", CAT => "Traffic_Category"],
+	 ["brunnels", "brunnels",
+	  NAME => "Brunnel_Comment", CAT => "Brunnel_Category"],
+	 [["comments_cyclepath", "radwege_exact"], "radwege",
+	  NAME => "Cyclepath_Comment", CAT => "Cyclepath_Category"],
+	 ["comments_ferry", "comments_ferry",
+	  NAME => "Ferry_Comment", CAT => "Ferry_Category"],
+	 ["comments_kfzverkehr", "comments_kfzverkehr",
+	  NAME => "Traffic_Comment", CAT => "Traffic_Category"],
+	 ["comments_misc", "comments_misc",
+	  NAME => "Comment", CAT => "Comment_Category"],
+	 ["comments_mount", "comments_mount",
+	  NAME => "Mount_Comment", CAT => "Mount_Category"],
+	 ["comments_path", "comments_path",
+	  NAME => "Path_Comment", CAT => "Path_Category"],
+	 ["comments_route", "comments_route",
+	  NAME => "Route_Comment", CAT => "Route_Category"],
+	 ["comments_tram", "comments_tram",
+	  NAME => "Tram_Comment", CAT => "Tram_Category"],
+	 ["gesperrt", "gesperrt",
+	  NAME => "Blocking_Comment", CAT => "Blocking_Category"],
+	 ["green", "green",
+	  NAME => "Green_Comment", CAT => "Green_Category"],
+	 [[$if_city->("handicap_s"), $if_region->("handicap_l")], "handicap",
+	  NAME => "Handicap_Comment", CAT => "Handicap_Category"],
+	 ["hoehe", "hoehe",
+	  NAME => "Elevation"],
+	 ["nolighting", "nolighting",
+	  NAME => "Nolighting_Comment", CAT => "Nolighting_Category"],
+	 [[$if_city->("qualitaet_s"), $if_region->("qualitaet_l")], "qualitaet",
+	  NAME => "Quality_Comment", CAT => "Quality_Category"],
+	 [["orte", "orte2"], "orte",
+	  NAME => "Place_Name", NAME_ADD => "Place_AddName",
+	  CAT => "Place_Category"],
+	 ["sehenswuerdigkeit", "sehenswuerdigkeit",
+	  NAME => "Sight_Name", CAT => "Sight_Category"],
+	 # missing: vorfahrt
+	);
+    # Returns: strassen files, label, dest filename, mapping
+    my $getdef = sub {
+	my $def = Storable::dclone($_[0]);
+	my @ret;
+	push @ret, shift @$def;
+	my $label = $ret[-1];
+	$label = $label->[0] if ref $label eq 'ARRAY';
+	push @ret, $label;
+	push @ret, shift @$def;
+	push @ret, $def;
+	@ret;
+    };
+
+    my $rgb = sub {
+	($_[0]<<16)+($_[1]<<8)+$_[2];
+    };
+
+    my %category_color =
+	(B  => $rgb->(255,0,0), # red
+	 HH => $rgb->(255,255,0),
+	 H  => $rgb->(255,255,0),
+	 N  => $rgb->(120,120,120),
+	 NN => $rgb->(0,200,0),
+
+	 Q0 => $rgb->(0,128,0),
+	 q0 => $rgb->(0,128,0),
+	 Q1 => $rgb->(0,180,0),
+	 q1 => $rgb->(0,180,0),
+	 Q2 => $rgb->(255,128,0),
+	 q2 => $rgb->(255,128,0),
+	 Q3 => $rgb->(255,0,0),
+	 q3 => $rgb->(255,0,0),
+	 Q4 => $rgb->(200,0,0),
+	 q4 => $rgb->(200,0,0),
+	 RW => $rgb->(0,0,200),
+	);
+    my $def_color = 0; # black
+    my %category_width =
+	(
+	 B  => 4,
+	 HH => 4,
+	 H  => 3,
+	 N  => 3,
+	 NN => 3,
+
+	 Q0 => 2,
+	 Q1 => 2,
+	 Q2 => 2,
+	 Q3 => 2,
+	 Q4 => 2,
+
+	 q0 => 1,
+	 q1 => 1,
+	 q2 => 1,
+	 q3 => 1,
+	 q4 => 1,
+
+	 RW => 3,
+	);
+    my $def_width = 1;
+    my %category_style =
+	(
+	 Q0 => 46,
+	 Q1 => 46,
+	 Q2 => 46,
+	 Q3 => 46,
+	 Q4 => 46,
+
+	 # Laengs mit Querstreifen: 26
+
+	 q0 => 46, # nur Querstreifen
+	 q1 => 46,
+	 q2 => 46,
+	 q3 => 46,
+	 q4 => 46,
+
+	 # 3 ist dotted
+	 RW => 4, # dashed (short)
+	);
+    my $def_style = 2; # solid
+    my %category_symbol =
+	("SW|IMG:airport.gif" => 52, # airport
+	 "SW|IMG:hospital.gif" => 56,
+	 "SW|IMG:church.gif" => 65, # church or monastir
+	 0 => 66, # place or church
+	 1 => 66, # place or church
+	 2 => 66, # place or church
+	 3 => 66, # place or church
+	 4 => 66, # place or church
+	 5 => 66, # place or church
+	 6 => 66, # place or church
+	);
+    my $def_symbol = 35; # star
+    my %category_fontsize =
+	(
+	);
+    my $def_fontsize = 12;
 
     my $mid = "";
-    my $mif = <<EOF;
+    my $mif = "";
+    my %mid;
+    my %mif;
+
+    my %column_to_index;
+    my @column_names;
+
+    my $onefile = $args{onefile} || 0;
+    if ($onefile) {
+	# single output file
+	@column_names =
+	    qw(Name Category
+	       Elevation
+	       Trafficlight_Comment Trafficlight_Category
+	       Blocking_Comment Blocking_Category
+	       Cyclepath_Comment Cyclepath_Category
+	       Quality_Comment Quality_Category
+	       Handicap_Comment Handicap_Category
+	       Mount_Comment Mount_Category
+	       Path_Comment Path_Category
+	       Route_Comment Route_Category
+	       Tram_Comment Tram_Category
+	       Traffic_Comment Traffic_Category
+	       Ferry_Comment Ferry_Category
+	       Brunnel_Comment Brunnel_Category
+	       Green_Comment Green_Category
+	       Nolighting_Comment Nolighting_Category
+	       Comment Comment_Category
+	       Place_Name Place_AddName Place_Category
+	       Sight_Name Sight_Category
+	      );
+	# XXX missing columns: Author, AcquireAuthor,CreationDate, AcquireDate
+	{
+	    my $i = 0;
+	    for (@column_names) {
+		$column_to_index{$_} = $i;
+		$i++;
+	    }
+	}
+
+	$mif = <<EOF;
 VERSION $version
 CHARSET "WindowsLatin1"
 DELIMITER ","
 ${coordsysline}COLUMNS @{[ scalar @column_names ]}
 EOF
-    for my $column (@column_names) {
-	$mif .= <<EOF;
+	for my $column (@column_names) {
+	    $mif .= <<EOF;
   $column [% TYPE_${column} %]
 EOF
-    }
-    $mif .= <<EOF;
+	}
+	$mif .= <<EOF;
+DATA
+
+EOF
+    } else {
+	# multiple output files
+	for my $def (@street_files) {
+	    my(undef, $label, undef, $field_map_ref) = $getdef->($def);
+	    my @field_map = @$field_map_ref;
+	    my %field_map = @field_map;
+	    $mif{$label} = <<EOF;
+VERSION $version
+CHARSET "WindowsLatin1"
+DELIMITER ","
+${coordsysline}COLUMNS @{[ scalar values %field_map ]}
+EOF
+	    for(my $i = 1; $i <= $#field_map; $i+=2) {
+		my $column = $field_map[$i];
+		$mif{$label} .= <<EOF;
+  $column [% TYPE_${column} %]
+EOF
+	    }
+	    $mif{$label} .= <<EOF;
 DATA
 
 EOF
 
+	    $mid{$label} = "";
+	}
+    }
+
     my($minx,$miny,$maxx,$maxy);
-    my @max_len;
+    my %max_len;
 
  FILELOOP:
-    for my $def ([["strassen", "landstrassen", "landstrassen2", "plaetze", "faehren"], NAME => "Name", CAT => "Category"],
-		 ["ampeln", NAME => "Trafficlight_Comment", CAT => "Traffic_Category"],
-		 ["brunnels", NAME => "Brunnel_Comment", CAT => "Brunnel_Category"],
-		 [["comments_cyclepath", "radwege_exact"], NAME => "Cyclepath_Comment", CAT => "Cyclepath_Category"],
-		 ["comments_ferry", NAME => "Ferry_Comment", CAT => "Ferry_Category"],
-		 ["comments_kfzverkehr", NAME => "Traffic_Comment", CAT => "Traffic_Category"],
-		 ["comments_misc", NAME => "Comment", CAT => "Comment_Category"],
-		 ["comments_mount", NAME => "Mount_Comment", CAT => "Mount_Category"],
-		 ["comments_path", NAME => "Path_Comment", CAT => "Path_Category"],
-		 ["comments_route", NAME => "Route_Comment", CAT => "Route_Category"],
-		 ["comments_tram", NAME => "Tram_Comment", CAT => "Tram_Category"],
-		 ["gesperrt", NAME => "Blocking_Comment", CAT => "Blocking_Category"],
-		 ["green", NAME => "Green_Comment", CAT => "Green_Category"],
-		 [["handicap_s", "handicap_l"], NAME => "Handicap_Comment", CAT => "Handicap_Category"],
-		 ["hoehe", NAME => "Elevation"],
-		 ["nolighting", NAME => "Nolighting_Comment", CAT => "Nolighting_Category"],
-		 [["qualitaet_s", "qualitaet_l"], NAME => "Quality_Comment", CAT => "Quality_Category"],
-		 # missing: vorfahrt
-		) {
-	my $file = shift @$def;
-	my %field_map = @$def;
-	while(my($k,$v) = each %field_map) {
-	    my $inx = $column_to_index{$v};
-	    if (!defined $inx) {
-		warn "Cannot find index for $v, skipping"; # XXX should die some day
-		next FILELOOP;
+    for my $def (@street_files) {
+	my($file, $label, undef, $field_map_ref) = $getdef->($def);
+	my @field_map = @$field_map_ref;
+	my %field_map = @field_map;
+	if ($onefile) {
+	    while(my($k,$v) = each %field_map) {
+		my $inx = $column_to_index{$v};
+		if (!defined $inx) {
+		    warn "Cannot find index for $v, skipping"; # XXX should die some day
+		    next FILELOOP;
+		}
+		$field_map{$k} = $inx;
 	    }
-	    $field_map{$k} = $inx;
+	} else {
+	    @column_names = ();
+	    my $inx = 0;
+	    for(my $i = 0; $i <= $#field_map; $i+=2) {
+		$field_map{$field_map[$i]} = $inx;
+		push @column_names, $field_map[$i+1];
+		$inx++;
+	    }
 	}
 
 	my $str;
-	eval {
-	    if (ref $file eq 'ARRAY') {
-		$str = MultiStrassen->new(@$file);
-		if ($args{v}) { print STDERR "@$file...\n" }
-	    } else {
+	if (ref $file eq 'ARRAY') {
+	    my @s;
+	    for my $file (@$file) {
+		eval {
+		    push @s, Strassen->new($file);
+		};
+		if ($@) {
+		    warn "$@, skipping $file";
+		}
+	    }
+	    if (!@s) {
+		warn "No file found, skipping MultiStrassen completely...";
+		next;
+	    }
+	    $str = MultiStrassen->new(@s);
+	    if ($args{v}) { print STDERR "@$file...\n" }
+	} else {
+	    eval {
 		$str = Strassen->new($file);
 		if ($args{v}) { print STDERR "$file...\n" }
 	    };
-	};
-	if (!$str) {
-	    warn "$@, skipping $file...";
-	    next;
+	    if (!$str) {
+		warn "$@, skipping $file...";
+		next;
+	    }
 	}
 
 	my($this_minx, $this_miny, $this_maxx, $this_maxy) = $str->bbox;
@@ -459,45 +657,103 @@ EOF
 	$maxx = $this_maxx if !defined $maxx || $this_maxx > $maxx;
 	$maxy = $this_maxy if !defined $maxy || $this_maxy > $maxy;
 
+	my $out_mif = $onefile ? \$mif : \$mif{$label};
+	my $out_mid = $onefile ? \$mid : \$mid{$label};
+
 	$str->init;
 	while(1) {
 	    my $r = $str->next;
-	    my $no_coords = @{ $r->[Strassen::COORDS()] };
+	    my $coords = $r->[Strassen::COORDS()];
+	    my $no_coords = @$coords;
 	    last if !$no_coords;
 
+	    my $cat = $r->[Strassen::CAT()];
+	    my $name = $r->[Strassen::NAME()];
+	    my $addname;
+	    if ($name =~ /(.*)\|(.*)/) {
+		($name, $addname) = ($1, $2);
+	    }
+
 	    if ($no_coords == 1) {
-		my($x, $y) = split /,/, $r->[Strassen::COORDS()]->[0];
+		my($x, $y) = split /,/, $coords->[0];
 		($x,$y) = $trim_accuracy->($conv->($x,$y)) if $conv;
-		$mif .= "Point $x $y\n";
-	    } else {
-		$mif .= "Pline $no_coords\n";
-		for my $p (@{ $r->[Strassen::COORDS()] }) {
+		$$out_mif .= "Point $x $y\n";
+
+		my $color = $category_color{$cat};
+		$color = $def_color if !defined $color;
+		my $symbol = $category_symbol{$cat};
+		$symbol = $def_symbol if !defined $symbol;
+		my $fontsize = $category_fontsize{$cat};
+		$fontsize = $def_fontsize if !defined $fontsize;
+
+		$$out_mif .= "    Symbol ($symbol,$color,$fontsize)\n";
+	    } elsif ($cat =~ /^F:(.*)/) {
+		$cat = $1;
+
+		$$out_mif .= "Region 1\n  $no_coords\n";
+		for my $p (@$coords) {
 		    my($x, $y) = split /,/, $p;
 		    ($x,$y) = $trim_accuracy->($conv->($x,$y)) if $conv;
-		    $mif .= "$x $y\n";
+		    $$out_mif .= "$x $y\n";
 		}
-		$mif .= "    Pen (1,2,1)\n";
+
+		my $color = $category_color{$cat};
+		$color = $def_color if !defined $color;
+		if ($cat =~ /^#(..)(..)(..)/) {
+		    $color = $rgb->(hex($1), hex($2), hex($3));
+		}
+
+		$$out_mif .= "    Pen (1,2,0)\n";
+		$$out_mif .= "    Brush (2,$color,16777215)\n"; # solid fill
+
+	    } else {
+		$$out_mif .= "Pline $no_coords\n";
+		for my $p (@$coords) {
+		    my($x, $y) = split /,/, $p;
+		    ($x,$y) = $trim_accuracy->($conv->($x,$y)) if $conv;
+		    $$out_mif .= "$x $y\n";
+		}
+
+		my $color = $category_color{$cat};
+		$color = $def_color if !defined $color;
+		my $style = $category_style{$cat};
+		$style = $def_style if !defined $style;
+		my $width = $category_width{$cat};
+		$width = $def_width if !defined $width;
+
+		$$out_mif .= "    Pen ($width,$style,$color)\n";
 	    }
 
 	    my @data_row = ("") x @column_names;
 	    if (exists $field_map{"NAME"}) {
 		my $data_row_inx = $field_map{"NAME"};
-		$data_row[$data_row_inx] = $r->[Strassen::NAME()];
-		$max_len[$data_row_inx] = length $r->[Strassen::NAME()]
-		    if (!defined $max_len[$data_row_inx] ||
-			$max_len[$data_row_inx] < length $r->[Strassen::NAME()]
+		my $column_name = $column_names[$data_row_inx];
+		$data_row[$data_row_inx] = $name;
+		$max_len{$column_name} = length $name
+		    if (!exists $max_len{$column_name} ||
+			$max_len{$column_name} < length $name
+		       );
+	    }
+	    if (exists $field_map{"NAME_ADD"}) {
+		my $data_row_inx = $field_map{"NAME_ADD"};
+		my $column_name = $column_names[$data_row_inx];
+		$data_row[$data_row_inx] = $addname;
+		$max_len{$column_name} = length $addname
+		    if (!exists $max_len{$column_name} ||
+			$max_len{$column_name} < length $addname
 		       );
 	    }
 	    if (exists $field_map{"CAT"}) {
 		my $data_row_inx = $field_map{"CAT"};
-		$data_row[$data_row_inx] = $r->[Strassen::CAT()];
-		$max_len[$data_row_inx] = length $r->[Strassen::CAT()]
-		    if (!defined $max_len[$data_row_inx] ||
-			$max_len[$data_row_inx] < length $r->[Strassen::CAT()]
+		my $column_name = $column_names[$data_row_inx];
+		$data_row[$data_row_inx] = $cat;
+		$max_len{$column_name} = length $cat
+		    if (!exists $max_len{$column_name} ||
+			$max_len{$column_name} < length $cat
 		       );
 	    }
 	    $_ =~ s/\"//g for @data_row; # XXX better solution
-	    $mid .= join(",", map { qq{"$_"} } @data_row) . "\n";
+	    $$out_mid .= join(",", map { qq{"$_"} } @data_row) . "\n";
 	}
     }
 
@@ -505,25 +761,38 @@ EOF
     ($maxx,$maxy) = $trim_accuracy->($conv->($maxx,$maxy)) if $conv;
 
     my %t_args;
-    {
-	my $i = 0;
-	for (@column_names) {
-	    $t_args{"TYPE_$_"} = "Char(" . ($max_len[$i]+1) . ")";
-	    $i++;
-	}
+    while(my($key) = each %max_len) {
+	$t_args{"TYPE_$key"} = "Char(" . ($max_len{$key}+1) . ")";
     }
     my $t = Template->new(DEBUG => 'undef');
     my $new_mif;
-    $t->process(\$mif,
-		{
-		 minx => $minx,
-		 miny => $miny,
-		 maxx => $maxx,
-		 maxy => $maxy,
-		 %t_args,
-		}, \$new_mif);
+    my %new_mif;
+    my %new_mid;
+    my %tpl_vars = (minx => $minx,
+		    miny => $miny,
+		    maxx => $maxx,
+		    maxy => $maxy,
+		    %t_args,
+		   );
 
-    ($new_mif, $mid);
+    if ($onefile) {
+	$t->process(\$mif, { %tpl_vars }, \$new_mif)
+	    or warn $t->error;
+    } else {
+	for my $def (@street_files) {
+	    my(undef, $label, $destfile, undef) = $getdef->($def);
+	    $new_mif{$destfile} = "";
+	    $t->process(\$mif{$label}, { %tpl_vars }, \$new_mif{$destfile})
+		or warn $t->error;
+	    $new_mid{$destfile} = $mid{$label};
+	}
+    }
+
+    if ($onefile) {
+	($new_mif, $mid);
+    } else {
+	(\%new_mif, \%new_mid);
+    }
 }
 
 =item export_datadir($data_directory, $filename, %args)
@@ -535,12 +804,29 @@ Like export(), but use a data directory instead.
 sub export_datadir {
     my($data_directory, $filename, %args) = @_;
     my($mif, $mid) = create_mif_mid_from_data_directory($data_directory, %args);
-    open(MIF, ">$filename.MIF") or die "Can't create $filename.MIF: $!";
-    print MIF $mif;
-    close MIF;
-    open(MID, ">$filename.MID") or die "Can't create $filename.MID: $!";
-    print MID $mid;
-    close MID;
+    if (ref $mif eq 'HASH') {
+	while(my($file, $data) = each %$mif) {
+	    my $mid_data = $mid->{$file};
+	    if ($mid_data eq "") {
+		warn "MID for $file is empty, skipping...\n";
+		next;
+	    }
+	    open(MIF, ">${filename}_${file}.MIF") or die "Can't create ${filename}_${file}.MIF: $!";
+	    print MIF $data;
+	    close MIF;
+
+	    open(MID, ">${filename}_${file}.MID") or die "Can't create ${filename}_${file}.MID: $!";
+	    print MID $mid_data;
+	    close MID;
+	}
+    } else {
+	open(MIF, ">$filename.MIF") or die "Can't create $filename.MIF: $!";
+	print MIF $mif;
+	close MIF;
+	open(MID, ">$filename.MID") or die "Can't create $filename.MID: $!";
+	print MID $mid;
+	close MID;
+    }
 }
 
 =back
@@ -552,7 +838,7 @@ return 1 if caller;
 require Getopt::Long;
 my $use_datadir;
 my %args;
-if (!Getopt::Long::GetOptions(\%args, "o=s", "map=s", "tomap=s", "v!")) {
+if (!Getopt::Long::GetOptions(\%args, "o=s", "scope=s", "onefile!", "map=s", "tomap=s", "v!")) {
     die "usage!";
 }
 my $o = delete $args{o};
