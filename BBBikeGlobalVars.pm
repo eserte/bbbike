ea59bf1926dc1f86909d41a5b81e359d5097fc38
eserte <eserte>
2004-03-17 02:06:35 +0000

    @edit_mode_standard_cmd

--- BBBikeGlobalVars.pm
+++ BBBikeGlobalVars.pm
@@ -81,6 +81,7 @@ use vars qw(
      $plotstr_draw_sub $plotpoint_draw_sub $plotorte_draw_sub $progress
      @remember_plot_str @remember_plot_p
      @standard_mode_cmd @edit_mode_cmd @edit_mode_b_cmd @edit_mode_brb_cmd
+     @edit_mode_standard_cmd
      $map_mode %map_mode_callback $map_mode_deactivate $b2_mode %b2_mode_desc
      %set_route_point %alt_set_route_point $search_stat $search_visual
      $coord_prefix $use_current_coord_prefix %delayed_sub_timer
