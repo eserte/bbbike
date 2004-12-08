041ed06d5cc31ab1fed65f16edeb184044c8e7d2
eserte <eserte>
2004-12-08 00:39:36 +0000

    *** empty log message ***

---
+++ data/temp_blockings/bbbike-temp-blockings-optimized.pl
@@ -0,0 +1,254 @@
+@temp_blocking = ();
+$#temp_blocking = 114;
+push @temp_blocking,
+(
+       {
+         'until' => 1103151600,
+         'text' => 'K 7318; (Pinnow-L 24-Haßleben); OD Buchholz Kanal- und Straßenbau Vollsperrung 07.09.2004-15.12.2004 ',
+         'from' => 1094421600,
+         'data' => 'userdel	q4 32334,89385 31796,89304
+',
+         'type' => 'handicap'
+       },
+       undef,
+       undef,
+       undef,
+       {
+         'until' => 1104620400,
+         'text' => 'B 96a; (Bahnhofstr., Hauptstr.); OD Schildow Kanal- und Straßenbau Vollsperrung 20.09.2004-01.01.2005 ',
+         'from' => 1095544800,
+         'data' => 'userdel	q4 8493,25378 8370,25539
+userdel	q4 8370,25539 8182,25608
+',
+         'type' => 'handicap'
+       },
+       undef,
+       undef,
+       undef,
+       {
+         'until' => 1114898400,
+         'text' => 'L 40; (Großbeeren-Güterfelde); zw. Großbeeren u. Neubeeren Neubau Bauwerk; Vollsperrung; 24.09.2004-30.04.2005 ',
+         'from' => 1095890400,
+         'data' => 'userdel	2 2715,-6365 2443,-6309
+',
+         'type' => 'gesperrt'
+       },
+       {
+         'until' => 1103929200,
+         'text' => 'K 6907; (B 2-AS Ferch); OD Neuseddin Straßenbauarbeiten Vollsperrung 04.10.2004-24.12.2004 ',
+         'from' => 1096754400,
+         'data' => 'userdel	q4 -18080,-12637 -17374,-13449
+',
+         'type' => 'handicap'
+       },
+       undef,
+       {
+         'until' => 1102460400,
+         'text' => 'B 167; zw. Bad Freienw. u. Falkenberg, Höhe Papierfabrik Neubau von Durchlässen Vollsperrung 04.10.2004-07.12.2004 ',
+         'from' => 1096754400,
+         'data' => 'userdel	q4 49039,44131 48583,44366
+userdel	q4 49039,44131 49691,43812
+',
+         'type' => 'handicap'
+       },
+       undef,
+       {
+         'until' => 1112306400,
+         'text' => 'K 6904; (Gröben-Nudow); OD Fahlhorst, Dorfstr. Straßenbauarbeiten Vollsperrung 04.10.2004-31.03.2005 ',
+         'from' => 1096754400,
+         'data' => 'userdel	q4 -5452,-11456 -5709,-10987
+',
+         'type' => 'handicap'
+       },
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       undef,
+       {
+         'until' => 1104534000,
+         'text' => 'Naumannstr. (Schöneberg) in beiden Richtungen zwischen Torgauer Str. und Tempelhofer Weg Brückenarbeiten, Straße vollständig gesperrt (bis 31.12.2004)',
+         'from' => 1098132674,
+         'data' => 'userdel	2 7716,8048 7717,7759
+',
+         'type' => 'gesperrt'
+       },
+       undef,
+       undef,
+       undef,
+       {
+         'until' => 1102719600,
+         'text' => 'L 16; (Siedl.Schönwalde-Pausin); Bahnübergang Gleisbauarbeiten Vollsperrung; Umleitung 25.10.2004-10.12.2004 ',
+         'from' => 1098568800,
+         'data' => 'userdel	2 -10559,23255 -10737,23418
+',
+         'type' => 'gesperrt'
+       },
+       {
+         'until' => 1112306400,
+         'text' => 'B 101; (Luckenwalder-, Berliner Str.); OD Trebbin Straßenbauarbeiten Vollsperrung 27.10.2004-31.03.2005 ',
+         'from' => 1098741600,
+         'data' => 'userdel	q4 -1623,-21150 -1887,-21501
+',
+         'type' => 'gesperrt'
+       },
+       undef,
+       {
+         'until' => 1104534000,
+         'text' => 'L 302; (Schöneicher Straße in Schöneiche); zwischen Stegweg bis Dorfaue: Gleisbau, Vollsperrung, 25.10.2004-31.12.2004 ',
+         'from' => 1098568800,
+         'data' => 'userdel	q4 31221,8312 30688,8432
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1103842800,
+         'text' => 'L 30; (Schönower Chaussee); zwischen Weinbergstraße und Edelweißstraße: Straßen- u. Radwegebau, Vollsperrung, 26.10.2004-23.12.2004 ',
+         'from' => 1098655200,
+         'data' => 'userdel	q4 21637,30946 20794,30899
+userdel	q4 21637,30946 21955,30976
+',
+         'type' => 'handicap'
+       },
+       undef,
+       undef,
+       {
+         'until' => 1103410800,
+         'text' => 'L 171; (Hohen Neuendorf-Hennigsdorf); Bereich Anschlußstelle Straßenbau Vollsperrung 28.10.2004-18.12.2004 ',
+         'from' => 1098828000,
+         'data' => 'userdel	2 -926,27132 -2118,26060
+',
+         'type' => 'gesperrt'
+       },
+       undef,
+       {
+         'until' => 1102287540,
+         'text' => 'Rixdorfer Weihnachtsmarkt, 03.12.2004-05.12.2004',
+         'from' => 1101942000,
+         'file' => 'rixdorfer_weihnachtsmarkt.bbd',
+         'type' => 'gesperrt'
+       },
+       undef,
+       {
+         'until' => 1104620400,
+         'text' => 'L 21; (Mühlenbecker Str.); OL Schildow grundh. Straßenbau Vollsperrung 16.08.2004-01.01.2005 ',
+         'from' => 1092520800,
+         'data' => 'userdel	q4 8231,26584 8194,25966
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1114898400,
+         'text' => 'L 401; (Lindenallee); OL Zeuthen, zw. Platanenallee und Ahornallee grundhafter Straßenbau Vollsperrung 01.11.2004-30.04.2005 ',
+         'from' => 1099177200,
+         'data' => 'userdel	q4 26581,-7087 26146,-6218
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1114898400,
+         'text' => 'K 6740; (L 38 östl. Berkenbrück-Steinhöfel); OL Demnitz Straßenbauarbeiten Vollsperrung 12.07.2004-30.04.2005 ',
+         'from' => 1089496800,
+         'data' => 'userdel	q4 64514,-1544 64439,-1243
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1114898400,
+         'text' => 'L 19; (Zechlinerhütte-Wesenberg (MVP)); zw. Abzw. Klein Zerlang u. LG (nö. Prebelowbrücke) Brückenneubau Vollsperrung 11.10.2004-30.04.2005 ',
+         'from' => 1097359200,
+         'data' => 'userdel	2 -26403,85177 -26316,84900
+',
+         'type' => 'gesperrt'
+       },
+       {
+         'until' => 1102719600,
+         'text' => 'L 166; (B 5-Friesack-Garz); zw. B 5 u. Friesack u. KG nördl. Zootzen Straßenbauarbeiten Vollsperrung 22.11.2004-10.12.2004 ',
+         'from' => 1100991600,
+         'data' => 'userdel	2 -45632,38429 -46170,36687
+',
+         'type' => 'gesperrt'
+       },
+       undef,
+       {
+         'until' => 1103583600,
+         'text' => 'L 23; (Templin-Lychen); OD Lychen Straßenbau Vollsperrung 17.11.2004-20.12.2004 ',
+         'from' => 1100559600,
+         'data' => 'userdel	q4 3125,88753 2788,89447
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1103151599,
+         'text' => 'Volkradstr. (Friedrichsfelde) in Richtung Rummelsburger Str. ab Sewanstr. Baustelle, Fahrtrichtung gesperrt, Umleitung: Sewanstr. - Rummelsburger Str. (bis Mitte 12.2004)',
+         'from' => 1100942003,
+         'data' => 'userdel	q4; 17427,10259 17621,10994
+',
+         'type' => 'handicap'
+       },
+       undef,
+       {
+         'until' => 1114898400,
+         'text' => 'Ebertstr. Richtung Potsdamer Platz zwischen Behrensstr. und Hannah-Ahrendt-Str. Baustelle, Fahrtrichtung gesperrt (bis April 2005)',
+         'from' => 1101507035,
+         'data' => 'userdel	q4; 8595,12066 8571,11846
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1102374000,
+         'text' => 'B 167; (Zerpenschleuse-Liebenwalde); zw. OA Liebenwalde und Abzw. Hammer Erneuerung Durchlass Vollsperrung 26.11.2004-06.12.2004 ',
+         'from' => 1101337200,
+         'data' => 'userdel	2 11228,52175 9686,52037
+',
+         'type' => 'gesperrt'
+       },
+       {
+         'until' => 1103583600,
+         'text' => 'L 75; (Karl-Marx-Straße); OD Großziethen, von Dorfstraße bis Friedhofsweg Straßenbauarbeiten Vollsperrung 24.05.2004-20.12.2004 ',
+         'from' => 1085263200,
+         'data' => 'userdel	q4 13225,-681 13309,-1268
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1102287600,
+         'text' => 'Sophienstraße zwischen Große Hamburger Straße und Rosenthaler Straße Umwelt- und Weihnachtsmarkt, für den Fahrzeugverkehr gesperrt. Dauer 04.12.2004, 06.00 Uhr bis 05.12.2004, 24.00 Uhr ',
+         'from' => 1102050000,
+         'data' => 'userdel	2 9982,13411 10312,13231
+',
+         'type' => 'gesperrt'
+       },
+       undef,
+       undef,
+       {
+         'until' => 1103065200,
+         'text' => 'L 912; (Päwesin-Gortz); Brücke über Seeverbindung bei Päwesin Brückenneubau Vollsperrung 26.11.2004-14.12.2004 ',
+         'from' => 1101337200,
+         'data' => 'userdel	2 -37026,11176 -36427,10861
+',
+         'type' => 'gesperrt'
+       },
+       {
+         'until' => 1102978800,
+         'text' => 'Emmentaler Str. (Reinickendorf) Richtung Westen zwischen Residenzstr. und Gamsbartweg Baustelle, Straße Richtung Westen gesperrt, Einbahnstraßenregelung Richtung Osten (bis 13.12.2004)',
+         'from' => 1101803392,
+         'data' => 'userdel	q4; 7693,18481 7350,18262
+',
+         'type' => 'handicap'
+       },
+       {
+         'until' => 1117576800,
+         'text' => 'Akeleiweg, Tiefbauarbeiten, Straße von Eisenhutweg in Richtung Stubenrauchstraße gesperrt, Dauer: bis 31.05.2005 ',
+         'from' => 1101934006,
+         'data' => 'userdel	q4; 17894,2783 17631,3200 17603,3240 17388,3576
+',
+         'type' => 'handicap'
+       }
+     
+);
---
+++ t/bbbikevar.t
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+# -*- perl -*-
+
+#
+# $Id: bbbikevar.t,v 1.1 2004/11/29 22:08:58 eserte Exp eserte $
+# Author: Slaven Rezic
+#
+
+use strict;
+
+use File::Basename;
+
+use FindBin;
+use lib "$FindBin::RealBin/..";
+use BBBikeVar;
+
+BEGIN {
+    if (!eval q{
+	use Test::More;
+	1;
+    }) {
+	print "1..0 # skip: no Test::More module\n";
+	exit;
+    }
+}
+
+BEGIN { plan tests => 1 }
+
+my $ports_dir = "/usr/ports";
+my $bbbike_bsd_port = $ports_dir . "/german/BBBike";
+
+SKIP: {
+    skip("No BSD port for BBBike available on this system", 1)
+	if ! -d $bbbike_bsd_port;
+    chdir $bbbike_bsd_port or die "Can't chdir to $bbbike_bsd_port: $!";
+    my($out) = `make fetch-list DISTDIR=/`;
+    my @url;
+    while($out =~ m{((?:ftp|http)://\S+)}g) {
+	push @url, $1;
+    }
+    my %dir_url = map { (dirname($_),1) } @url;
+    if (!exists $dir_url{$BBBike::DISTDIR}) {
+	fail("$BBBike::DISTDIR not found in " . join(", ", keys %dir_url));
+    } else {
+	pass("$BBBike::DISTDIR found");
+    }
+}
+
+__END__
