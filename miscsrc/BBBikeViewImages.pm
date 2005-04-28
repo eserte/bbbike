f9333185f4ab578e309d57a2b2310cbd07cc1f6f
eserte <eserte>
2005-04-28 20:56:44 +0000

    fixes

--- miscsrc/BBBikeViewImages.pm
+++ miscsrc/BBBikeViewImages.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: BBBikeViewImages.pm,v 1.1 2005/04/28 20:53:32 eserte Exp $
+# $Id: BBBikeViewImages.pm,v 1.2 2005/04/28 20:56:44 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2005 Slaven Rezic. All rights reserved.
@@ -14,7 +14,7 @@ push @ISA, "BBBikePlugin";
 
 use strict;
 use vars qw($VERSION $image_viewer_toplevel $image_viewer_label);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
 
 sub register {
     add_button();
@@ -61,23 +61,23 @@ sub button {
 	    $image_viewer_label = $image_viewer_toplevel->Label->pack(-fill => "both", -expand => 1);
 	    $image_viewer_toplevel->OnDestroy
 		(sub {
-		     my $p = $image_viewer_toplevel->Subwidget("photo");
+		     my $p = $image_viewer_toplevel->{"photo"};
 		     if ($p) {
 			 $p->delete;
 		     }
 		 });
 	}
-	my $p = $image_viewer_toplevel->Subwidget("photo");
+	my $p = $image_viewer_toplevel->{"photo"};
 	if ($p) {
 	    $p->delete;
-	    $image_viewer_toplevel->Advertise(photo => undef);
+	    $image_viewer_toplevel->{"photo"} = undef;
 	}
 	$p = main::image_from_file($main::top, $abs_file);
 	if (!$p) {
 	    main::status_message("Kann die Datei $abs_file nicht als Bild interpretieren", "die");
 	}
 	$image_viewer_label->configure(-image => $p);
-	$image_viewer_toplevel->Advertise(photo => $p);
+	$image_viewer_toplevel->{"photo"} = $p;
     }
 }
 
