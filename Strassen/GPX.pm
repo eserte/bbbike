7da125d72604d68677cef00623ba815dd29e849e
eserte <eserte>
2005-10-25 22:01:41 +0000

    *** empty log message ***

--- Strassen/GPX.pm
+++ Strassen/GPX.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: GPX.pm,v 1.1 2005/07/17 20:39:22 eserte Exp $
+# $Id: GPX.pm,v 1.1 2005/07/17 20:39:22 eserte Exp eserte $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2005 Slaven Rezic. All rights reserved.
@@ -85,6 +85,67 @@ sub latlong2xy {
     my $lon = $node->findvalue(q{./@lon});
     my($x, $y) = $Karte::Standard::obj->trim_accuracy($Karte::Polar::obj->map2standard($lon, $lat));
     ($x, $y);
+}
+
+sub xy2longlat {
+    my($c) = @_;
+    my($lon, $lat) = $Karte::Polar::obj->trim_accuracy($Karte::Polar::obj->standard2map(split /,/, $c));
+    ($lon, $lat);
+}
+
+sub bbd2gpx {
+    my($self) = @_;
+    require XML::LibXML;
+    $self->init;
+    my @wpt;
+    my @trkseg;
+    while(1) {
+	my $r = $self->next;
+	last if !@{ $r->[Strassen::COORDS] };
+	if (@{ $r->[Strassen::COORDS] } == 1) {
+	    push @wpt, { name => $r->[Strassen::NAME],
+			 coords => [ xy2longlat($r->[Strassen::COORDS][0]) ],
+		       };
+	} else {
+	    push @trkseg,
+		{
+		 name => $r->[Strassen::NAME],
+		 coords => [ map { [ xy2longlat($_) ] } @{ $r->[Strassen::COORDS] } ],
+		};
+	}
+    }
+    my $dom = XML::LibXML::Document->new('1.0', 'UTF8');
+    my $gpx = $dom->createElement("gpx");
+    $dom->setDocumentElement($gpx);
+    $gpx->setAttribute("version", "1.1");
+    $gpx->setAttribute("create", "http://www.bbbike.de");
+    for my $wpt (@wpt) {
+	my $wptxml = $gpx->addNewChild(undef, "wpt");
+	$wptxml->setAttribute("lat", $wpt->{coords}[1]);
+	$wptxml->setAttribute("lon", $wpt->{coords}[0]);
+	my $namexml = $wptxml->addNewChild(undef, "name");
+	$namexml->appendText($wpt->{name});
+    }
+    if (@trkseg) {
+	my $trkxml = $gpx->addNewChild(undef, "trk");
+	my $name_from = $trkseg[0]->{name};
+	my $name_to   = $trkseg[-1]->{name};
+	my $name = $name_from;
+	if ($name_from ne $name_to) {
+	    $name .= " - $name_to";
+	}
+	my $namexml = $trkxml->addNewChild(undef, "name");
+	$namexml->appendText($name);
+	for my $trkseg (@trkseg) {
+	    my $trksegxml = $trkxml->addNewChild(undef, "trkseg");
+	    for my $wpt (@{ $trkseg->{coords} }) {
+                my $trkptxml = $trksegxml->addNewChild(undef, "trkpt");
+                $trkptxml->setAttribute("lat", $wpt->[1]);
+                $trkptxml->setAttribute("lon", $wpt->[0]);
+            }
+	}
+    }
+    $dom->toString;
 }
 
 1;
