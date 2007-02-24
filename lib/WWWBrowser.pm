13f7023884e029e4aa4c786134e4752f3e0765ab
eserte <eserte>
2007-02-24 13:27:25 +0000

    *** empty log message ***

--- lib/WWWBrowser.pm
+++ lib/WWWBrowser.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: WWWBrowser.pm,v 2.35 2007/02/11 19:11:31 eserte Exp $
+# $Id: WWWBrowser.pm,v 2.36 2007/02/24 13:25:27 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 1999,2000,2001,2003,2005,2006 Slaven Rezic. All rights reserved.
@@ -21,7 +21,7 @@ use vars qw(@unix_browsers @available_browsers
 	    $VERSION $VERBOSE $initialized $os $fork
 	    $got_from_config $ignore_config);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 2.35 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 2.36 $ =~ /(\d+)\.(\d+)/);
 
 @available_browsers = qw(_debian_browser _internal_htmlview
 			 _default_gnome _default_kde
@@ -42,14 +42,10 @@ init();
 
 sub init {
     if (!$initialized) {
-	if (!defined $main::os) {
-	    $os = ($^O eq 'MSWin32' ? 'win' :
-		   $^O eq 'darwin'  ? 'macosx' :
-		   $^O eq 'MacOS'   ? 'mac' :
-		                      'unix');
-	} else {
-	    $os = $main::os;
-	}
+	$os = ($^O eq 'MSWin32' || $^O eq 'os2' || $^O eq 'dos' ? 'win' :
+	       $^O eq 'darwin'  ? 'macosx' :
+	       $^O eq 'MacOS'   ? 'mac' :
+	                          'unix');
 	if (!defined &main::status_message) {
 	    eval 'sub status_message { warn $_[0] }';
 	} else {
