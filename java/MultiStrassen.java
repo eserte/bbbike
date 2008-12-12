4794341640894501dd0447b0c29092ddcd825c26
eserte <eserte>
2008-12-12 17:50:55 +0000

    * comment about warning

--- java/MultiStrassen.java
+++ java/MultiStrassen.java
@@ -7,6 +7,15 @@ import java.util.Enumeration;
 import java.io.FileNotFoundException;
 //import Strassen;
 
+/* XXX Note that with jdk 1.5 this throws the warning
+
+   MultiStrassen.java:19: warning: [unchecked] unchecked call to addElement(E) as a member of the raw type java.util.Vector
+           Data.addElement(e2.nextElement());
+
+   The fix is to use generics (but this is probably not backward compatible).
+   See http://forums.sun.com/thread.jspa?threadID=584311
+*/
+
 class MultiStrassen extends Strassen {
   public MultiStrassen (Vector obj) throws Error, FileNotFoundException {
     for(Enumeration e = obj.elements(); e.hasMoreElements(); ) {
