a9bbc0a766fd318283839ab98af6983eeea34898
eserte <eserte>
2009-04-02 20:52:02 +0000

    * bbbike.radzeit.de -> bbbike.de

--- BBBikeDraw/BBBikeGoogleMaps.pm
+++ BBBikeDraw/BBBikeGoogleMaps.pm
@@ -28,7 +28,7 @@ if (!defined $bbbike_googlemaps_url) {
 	# Unfortunately I cannot use $BBBIKE_GOOGLEMAP_URL from BBBikeVar.pm here,
 	# because it seems that POSTs content is not sent through the rewriting
 	# rules...
-	$bbbike_googlemaps_url = "http://bbbike.radzeit.de/cgi-bin/bbbikegooglemap.cgi";
+	$bbbike_googlemaps_url = "http://bbbike.de/cgi-bin/bbbikegooglemap.cgi";
     }
 }
 
