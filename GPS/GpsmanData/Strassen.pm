6fcb29918b604fc735ca29183a02f624048848cd
eserte <eserte>
2009-04-02 20:29:16 +0000

    * as_wpt_strassen: also output a rough accuracy estimate

--- GPS/GpsmanData/Strassen.pm
+++ GPS/GpsmanData/Strassen.pm
@@ -1,10 +1,10 @@
 # -*- perl -*-
 
 #
-# $Id: Strassen.pm,v 1.1 2009/03/29 00:55:19 eserte Exp $
+# $Id: Strassen.pm,v 1.2 2009/04/02 20:29:16 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2008 Slaven Rezic. All rights reserved.
+# Copyright (C) 2008,2009 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -18,7 +18,7 @@ package GPS::GpsmanMultiData;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
 
 use Karte::Polar;
 use Karte::Standard;
@@ -35,7 +35,10 @@ sub as_wpt_strassen {
 	    my @t = gmtime($epoch); $t[4]++; $t[5]+=1900;
 	    my $isodate = sprintf "%04d%02d%02dT%02d%02d%02d", @t[5,4,3,2,1,0];
 	    my($x,$y) = $Karte::Standard::obj->trim_accuracy($Karte::Polar::obj->map2standard($wpt->Longitude, $wpt->Latitude));
-	    $s->push([$isodate, ["$x,$y"], "X"]);
+	    # XXX rough estimate...
+	    my $acc = ($wpt->Accuracy == 0 ? 10 :
+		       $wpt->Accuracy == 1 ? 25 : 50);
+	    $s->push(["$isodate acc=${acc}m", ["$x,$y"], "X"]);
 	}
     }
 
