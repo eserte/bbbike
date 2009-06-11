ebf9c22a7e5d434d11c7f2fb99e78a19d049cea7
eserte <eserte>
2009-06-11 21:36:02 +0000

    * the tmp files cleanup

---
+++ miscsrc/bbbike_tmp_cleanup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Cleanup plethora of temporary files created by bbbike or related
+# tools
+
+# Run only on development machines, not on live www machines, because
+# precious session data would be deleted.
+
+if [ "`hostname`" != "biokovo-amd64.herceg.de" ] 
+then
+    echo "Should only run on biokovo"
+    exit 1
+fi
+
+# Used for various cache files if no cache directory is provided (as
+# seems to be the case in some test scripts, in data/Makefile etc.,
+# maybe also in the cgi implementations)
+rm /tmp/*.cache
+rm /tmp/b_de_*
+rm /tmp/bbbike_b_de_*
+# Various temporary files created by data/Makefile
+rm /tmp/XXX_*-orig.bbd
+rm /tmp/fragezeichen_*.bbd
+# Probably files created by the bbbikedraw test scripts, to be checked XXX
+rm /tmp/bbbikedraw-mapserver-*
+rm /tmp/bbbikedraw_*
+# Temporary files for mapserver
+rm /tmp/bbbikems-xxx-*
+# Sessions for bbbike, precious on the live www machine
+rm -r /tmp/bbbike-sessions-*
+rm /tmp/bbbike-counter-*
+rm -r /tmp/coordssession
+# Temporary files for uploads for the cgi implementation
+rm /tmp/bbbike.cgi.upload.*
+# Sessions for wapbbbike
+rm /tmp/wapbbbike_sessions_*.db
+# Created by the group of newstreetform formulars, precious on the
+# live www machine
+rm /tmp/newstreetform-backup
+# Created by the tpage calls in mapserver/brb
+rm -r /tmp/bbbikettc
+# TODO: automatic mount calculation is broken, these are the files
+# with the "problematic" line segments:
+rm /tmp/problematic_*.bbd
+# Log of the cgpsmapper runs
+rm /tmp/cgpsmapper-images.log
+## Kept original files when using the bbbike editor. Not deleted here,
+## because it's likely that a bbbike session is still running
+# rm -r /tmp/bbbike-orig
