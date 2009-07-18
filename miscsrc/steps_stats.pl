81df87fe3ce415cdc9a1ac62e9bc6f1df7ff453e
eserte <eserte>
2009-07-18 22:18:07 +0000

    * steps_stats: added some manual statistical analysis into Pod

--- miscsrc/steps_stats.pl
+++ miscsrc/steps_stats.pl
@@ -54,3 +54,41 @@ print "-"x70,"\n";
 print join("\n", map { join("\t", @{$_}[0,1,2], $_->[3]->[Strassen::NAME]) } @res), "\n";
 
 __END__
+
+=pod
+
+   Daten:
+
+   * Spreetunnel oben auf der Treppe zwischen 12:09:44 und 12:09:59
+     unten ca. 12:10:17 oder so
+     wieder oben auf der anderen Seite 12:12:03
+     50+50 Stufen, unten im Tunnel 5 km/h (bei ca. 100m entspricht 72
+     Sekunden)
+     insgesamt also ca. 2:30 min verloren, davon 78 Sekunden beim
+     Treppensteigen
+     entspricht also 0.78 Sekunden/Stufe ~ 1s/Stufe
+
+   * Laut DIN-Norm 18065 beträgt die Steigung ~20cm. Laut
+     http://de.wikipedia.org/wiki/Spreetunnel_Friedrichshagen müssten
+     die Treppen 8.4m+1.5m=9.9m überwinden --- kommt ziemlich gut hin!
+     Mein GPS-Empfänger hat übrigens als tiefsten Punkt 27m angezeigt,
+     oben waren es ca. 35m, also hier nur ein Unterschied von 8m.
+
+   * Treppe am Rolandufer, 26 Stufen am
+     2005-06-25, wahrscheinlich mit Kind im Kindersitz: für die
+     Treppe brauchte ich geschätzte 40 Sekunden, Kind blieb
+     wahrscheinlich drinnen. -> 1.5s/Stufe
+
+   * Elsenbrücke: ca. 2 x 15 Stufen, insgesamt 60 Meter, im Mittel
+     verlorene Zeit 35 Sekunden. Bei 20 km/h hätte man es in ca. 10
+     Sekunden geschafft -> 0.83 Sekunden/Stufe
+
+   * Sasarsteig: mehrere Treppen, insgesamt 25 Stufen, im Mittel
+     verlorene Zeit 50 Sekunden. Bei 20 km/h hätte man es in ca. 21
+     Sekunden geschafft -> 0.6 Sekunden/Stufe. Kein Unterschied beim
+     Hoch/Runterfahren.
+
+   * Mühsamstr. 40 Meter, 34 Sekunden, unbekannte Anzahl von Stufen.
+     Bei 20 km/h hätte man 7 Sekunden dafür gebraucht.
+
+=cut
