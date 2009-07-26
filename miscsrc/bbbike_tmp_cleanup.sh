f715c70a1e887802465f96478c2f2b50454f6585
eserte <eserte>
2009-07-26 21:03:32 +0000

    * delete more * new option -all

--- miscsrc/bbbike_tmp_cleanup.sh
+++ miscsrc/bbbike_tmp_cleanup.sh
@@ -6,6 +6,21 @@
 # Run only on development machines, not on live www machines, because
 # precious session data would be deleted.
 
+deleteall=
+
+if [ $# != 0 ]
+then
+    case "$1" in
+        -all)
+	    deleteall=1
+            ;;
+        *)
+	    echo "usage: $0 [-all]"
+	    exit 1
+	    ;;
+    esac
+fi
+
 case "`hostname`" in
     biokovo-amd64.herceg.de | biokovo-amd64.rezic.de | mosor | devpc01-debian )
         ;;
@@ -21,12 +36,17 @@ esac
 rm /tmp/*.cache
 rm /tmp/b_de_*
 rm /tmp/bbbike_b_de_*
+# last * is a pid file - unfinished creation of cache file
+rm /tmp/bbbike_*.cache.*
 # Various temporary files created by data/Makefile
 rm /tmp/XXX_*-orig.bbd
 rm /tmp/fragezeichen_*.bbd
 # Probably files created by the bbbikedraw test scripts, to be checked XXX
 rm /tmp/bbbikedraw-mapserver-*
 rm /tmp/bbbikedraw_*
+rm /tmp/bbbikedraw.*
+# Images created by bbbike test scripts (-display option)
+rm /tmp/*_BBBikeViewImages
 # Temporary files for mapserver
 rm /tmp/bbbikems-xxx-*
 # Sessions for bbbike, precious on the live www machine
@@ -58,9 +78,20 @@ rm /tmp/??????????-bbbike-data.zip
 rm /tmp/Apache-Session-????????????????????????????????.lock
 # ???
 rm /tmp/bbbikems-*.bbd
-## Kept original files when using the bbbike editor. Not deleted here,
-## because it's likely that a bbbike session is still running
-# rm -r /tmp/bbbike-orig
-## Kept because possible precious information about failing tests
-# rm /tmp/??????????.bbbike_test.html
-# rm /tmp/??????????.bbbike_test
+## Created by BBBikeGPS
+rm /tmp/2???????.wpt-gpspoints.bbd-orig
+rm /tmp/2???????.trk-gpsspeed.bbd-orig
+rm /tmp/2???????.trk-gpspoints.bbd-orig
+rm /tmp/2???????.wpt-gpspoints.bbd
+rm /tmp/2???????.trk-gpsspeed.bbd
+rm /tmp/2???????.trk-gpspoints.bbd
+
+if [ "$deleteall" ]
+then
+    ## Original files when using the bbbike editor. Not deleted by default,
+    ## because it's likely that a bbbike session is still running
+    rm -r /tmp/bbbike-orig
+    ## Possible precious information about failing tests
+    rm /tmp/??????????.bbbike_test.html
+    rm /tmp/??????????.bbbike_test
+fi
