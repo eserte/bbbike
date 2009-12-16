8c2de35d9eafd641debad7d24f927dcdd8223bb1
Slaven Rezic <slaven@rezic.de>
2009-12-16 22:59:28 +0100

    * cease possible agrep warnings (if no $ENV{HOME} is available)

--- PLZ.pm
+++ PLZ.pm
@@ -294,6 +294,11 @@ sub look {
 	warn "About to call <@cmd>" if $VERBOSE;
 	CORE::open(PLZ, "-|") or do {
 	    $ENV{LANG} = $ENV{LC_ALL} = $ENV{LC_CTYPE} = 'C';
+	    # agrep emits some warnings "using working-directory '...'
+	    # to locate dictionaries" if it does not have a $ENV{HOME}
+	    # (which is probably a bug, because dictionaries are not
+	    # used at all)
+	    $ENV{HOME} = "/something";
 	    exec @cmd;
 	    warn "While doing @cmd: $!";
 	    require POSIX;
