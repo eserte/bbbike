8b20b0ec45f0a395da453a5349743faa9bcaa5b4
Slaven Rezic <slaven@rezic.de>
2010-02-22 22:42:40 +0100

    * new plugin FahrinfoQuery

---
+++ miscsrc/FahrinfoQuery.pm
@@ -0,0 +1,196 @@
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2010 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+# Description (de): Eine Routensuche bei Fahrinfo vornehmen
+package FahrinfoQuery;
+
+use strict;
+use vars qw($VERSION @ISA);
+$VERSION = '0.01';
+
+use BBBikePlugin;
+push @ISA, 'BBBikePlugin';
+
+use vars qw($icon);
+
+use CGI qw();
+use Encode qw(encode);
+
+use BBBikeUtil qw(bbbike_root);
+use Strassen::MultiStrassen;
+
+# XXX use Msg.pm some day
+sub M ($) { $_[0] } # XXX
+sub Mfmt { sprintf M(shift), @_ } # XXX
+
+sub register {
+    my $pkg = __PACKAGE__;
+    $BBBikePlugin::plugins{$pkg} = $pkg;
+    _create_image();
+    add_button();
+}
+
+sub unregister {
+    my $pkg = __PACKAGE__;
+    return unless $BBBikePlugin::plugins{$pkg};
+    my $mf = $main::top->Subwidget("ModePluginFrame");
+    my $subw = $mf->Subwidget($pkg . '_action');
+    if (Tk::Exists($subw)) { $subw->destroy }
+    delete $BBBikePlugin::plugins{$pkg};
+}
+
+sub _create_image {
+    if (!defined $icon) {
+	# Got from: http://www.bvg.de/images/favicon.ico
+	# and scaled to 16x16 using ImageMagick/convert
+	$icon = $main::top->Photo
+	    (-format => 'gif',
+	     -data => <<EOF);
+R0lGODlhEAAQAPcAAB8cByclCCkkByklByglCCklCS4rBy0pCSwoCi0qCC8sCTEtCDAtCjMu
+DD05CTw5DEE+C0E/DEZADEhCDElCDUxFDUxHDU5GDEVCEEtIEFFLDlVSEFlWEVxWEV1YEWRf
+D2BZE2FYE2JbEWBcE2JgEGdiE2ZgFWlhEm5mFG5pFXZuF3lvFYV7F4iAF4qEGY+HGZKKHJWK
+GJeLGZeLGpmPG5WRHZyUGqCVHaOXHKKYG6OaH6idHqSaIKegHKmhH6yjHbClHrCnH7OpHraw
+H6+oIbCkILSpILSrIbmwIsS5IsW9I8i6IMi8I8/DJM3GItDCJNDGJdTIJtXKJdbIKNfMKdjI
+JNjMI9rPJd7PJ9jNKNrRJtvRKODTJ+TTJubWJebYKuTdKu/fLO/iKPPiK/HiLPLlK/XjLfTl
+LPXkLPXmLPfnLfrpLPvrLv3rKfzrK/3qK/3rKv3rK/7qKv7qK/7rKv7rK//qK//rKv/rK/zq
+LPzrLf3qLP3rLP3rLv7qLP7qLf7rLP7rLf/qLP/rLP7qLv7rLv3sKf3sK/7tKf7sKv7tK//u
+Kv/uK//vKv7sLP7tLP7tLf/sLP7tLv7uLP/uLf/vLP/vLf/uLv/uL//vLv/wLf/wLszMzAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAJwALAAAAAAQABAAAAj4AAcJHEiwoMGD
+BO3gsaNwEJ6HDhc29ONHkMU5gyzasUhxkJ04cerU+QMIkJ85gP78ARlSjhw/K/nwiQOnDh+T
+cgC9kUOH0aJEgSTB4QOJjyNGiNoQojOmyhQyZrrkQeNFT5gnWNYAurTDgIMSRS58EfIhSwgG
+FJpQqgQjgg8EPA4kYSHCBQQnUsQUyiQjAQkNWizUMNHiBAomOrhE0jSjgAcJW1SAqEBkxIof
+AXBoyhRjghIBQ3oMWEAlRQcwGGxUakSDwIYHVqIAyFDGiAIODY5gUnQlxw0obNIAWaJIDZIX
+Qc7c2XOo0qZHbgxVgvQGziRLlQz1CQgAOw==
+EOF
+    }
+}
+
+sub add_button {
+    my $mf = $main::top->Subwidget("ModePluginFrame");
+    return unless defined $mf;
+
+    my $button = $mf->Button(main::image_or_text($icon, 'Fahrinfo'),
+			     -command => sub { choose() },
+			    );
+    BBBikePlugin::replace_plugin_widget($mf, $button,
+					__PACKAGE__ . '_action');
+#     BBBikePlugin::add_to_global_plugins_menu
+# 	    (-title   => M("Kürzeste Rundreise"),
+# 	     -topmenu => [Radiobutton => M('Kürzester Rundreisen-Modus'),
+# 			  %radio_args,
+# 			 ],
+# 	    );
+    $main::balloon->attach($button, -msg => M"Fahrinfo")
+	if $main::balloon;
+}
+
+sub choose {
+    if (@main::search_route_points < 2) {
+	main::status_message(M"Es existiert keine Route", "error");
+	return;
+    }
+    my $start = $main::search_route_points[0]->[main::SRP_COORD()];
+    # XXX no support for via
+    my $goal  = $main::search_route_points[-1]->[main::SRP_COORD()];
+
+    my $osm_data_dir = bbbike_root . '/data_berlin_osm_bbbike';
+    if (!-d $osm_data_dir) {
+	main::status_message(M"Das Verzeichnis $osm_data_dir existiert nicht. Keine Haltestellensuche möglich.", "error");
+	return;
+    }
+
+    my $ms = MultiStrassen->new("$osm_data_dir/_oepnv",
+				"$osm_data_dir/ubahnhof",
+				"$osm_data_dir/sbahnhof",
+			       );
+
+    my $t = $main::top->Toplevel(-title => 'Fahrinfo');
+    $t->transient($main::top) if $main::transient;
+    my $f = $t->Frame->pack(qw(-fill x -expand 1));
+    my $start_lb = $f->Scrolled('Listbox',
+				-scrollbars => 'osoe',
+				-exportselection => 0,
+			       )->pack(qw(-side left -fill x));
+    my $goal_lb  = $f->Scrolled('Listbox',
+				-scrollbars => 'osoe',
+				-exportselection => 0,
+			       )->pack(qw(-side left -fill x));
+    my $b = $t->Button(-text => 'Search',
+		      )->pack(qw(-fill x));
+
+    my(@start_stops, @goal_stops);
+    for my $def ([\@start_stops, $start, $start_lb],
+		 [\@goal_stops,  $goal,  $goal_lb],
+		) {
+	my($stops, $coord, $lb) = @$def;
+	@$stops = @{ $ms->nearest_point($coord, FullReturn => 1, AllReturn => 1) };
+	if (!@$stops) {
+	    $lb->insert("end", "Nothing found!");
+	} else {
+	    for my $stop (@$stops) {
+		my $r = $stop->{StreetObj};
+
+		my $cat = $r->[Strassen::CAT()];
+		$cat =~ s{^([US])[ABC]}{$1}; # remove VBB zone
+
+		my $name;
+		if ($cat =~ m{^[US]$}) {
+		    $name .= "$cat ";
+		}
+		$name .= $r->[Strassen::NAME()];
+		$name .= " (Berlin)"; # XXX what about Potsdam? etc.?
+		if ($cat !~ m{^[US]$}) {
+		    $name .= " ($cat)";
+		}
+
+		$lb->insert('end', $name); # XXX show also distance, maybe on map?
+	    }
+	    $lb->selectionSet(0);
+	}
+    }
+
+    $b->configure(-command => sub {
+		      my($start_name) = $start_lb->get($start_lb->curselection);
+		      my($goal_name)  = $goal_lb->get($goal_lb->curselection);
+		      $_ =~ s{\s+\((Tram|Bus)\)$}{} for ($start_name, $goal_name);
+		      search($start_name, $goal_name);
+		  });
+}
+
+sub search {
+    my($start_name, $goal_name) = @_;
+    CGI->import('-oldstyle_urls');
+    $_ = encode("iso-8859-1", $_) for ($start_name, $goal_name); # XXX utf8 or latin1?
+    my $qs = CGI->new({from => $start_name, # XXX add " (Berlin)"? # XXX add !
+		       to   => $goal_name,  # XXX "
+		       REQ0JourneyStopsSA1 => 1,
+		       REQ0JourneyStopsZA1 => 1
+		      })->query_string;
+    start_browser("http://mobil.bvg.de/Fahrinfo/bin/query.bin/dox?" . $qs);
+}
+
+sub start_browser {
+    my($url) = @_;
+    main::status_message("Der WWW-Browser wird mit der URL $url gestartet.", "info");
+    require WWWBrowser;
+    WWWBrowser::start_browser($url);
+}
+
+1;
+
+__END__
