5d6765a832113a54c775716c14d5f771f48e4acc
Slaven Rezic <slaven@rezic.de>
2010-02-22 23:19:46 +0100

    * improving FahrinfoQuery   * layout nicely behaves when resizing   * use a new working "AllReturn" implementation   * show (Berlin) or (Potsdam) correctly   * short "Str." form   * show distance (as the bird flies)   * double click centers on map

--- miscsrc/FahrinfoQuery.pm
+++ miscsrc/FahrinfoQuery.pm
@@ -21,17 +21,21 @@ $VERSION = '0.01';
 use BBBikePlugin;
 push @ISA, 'BBBikePlugin';
 
-use vars qw($icon);
+use vars qw($icon %city_border_points);
 
 use CGI qw();
 use Encode qw(encode);
 
-use BBBikeUtil qw(bbbike_root);
+use BBBikeUtil qw(bbbike_root m2km);
 use Strassen::MultiStrassen;
+use Strassen::Util;
 
 # XXX use Msg.pm some day
 sub M ($) { $_[0] } # XXX
 sub Mfmt { sprintf M(shift), @_ } # XXX
+
+use vars qw($LIMIT_LB);
+$LIMIT_LB = 12;
 
 sub register {
     my $pkg = __PACKAGE__;
@@ -121,15 +125,19 @@ sub choose {
 
     my $t = $main::top->Toplevel(-title => 'Fahrinfo');
     $t->transient($main::top) if $main::transient;
-    my $f = $t->Frame->pack(qw(-fill x -expand 1));
+    my $f = $t->Frame->pack(qw(-fill both -expand 1));
     my $start_lb = $f->Scrolled('Listbox',
 				-scrollbars => 'osoe',
 				-exportselection => 0,
-			       )->pack(qw(-side left -fill x));
+				-width => 50,
+				-height => $LIMIT_LB,
+			       )->pack(qw(-side left -fill both -expand 1));
     my $goal_lb  = $f->Scrolled('Listbox',
 				-scrollbars => 'osoe',
 				-exportselection => 0,
-			       )->pack(qw(-side left -fill x));
+				-width => 50,
+				-height => $LIMIT_LB,
+			       )->pack(qw(-side left -fill both -expand 1));
     my $b = $t->Button(-text => 'Search',
 		      )->pack(qw(-fill x));
 
@@ -138,7 +146,8 @@ sub choose {
 		 [\@goal_stops,  $goal,  $goal_lb],
 		) {
 	my($stops, $coord, $lb) = @$def;
-	@$stops = @{ $ms->nearest_point($coord, FullReturn => 1, AllReturn => 1) };
+	#@$stops = @{ $ms->nearest_point($coord, FullReturn => 1, AllReturn => 1) };
+	@$stops = get_nearest($ms, $coord);
 	if (!@$stops) {
 	    $lb->insert("end", "Nothing found!");
 	} else {
@@ -153,21 +162,41 @@ sub choose {
 		    $name .= "$cat ";
 		}
 		$name .= $r->[Strassen::NAME()];
-		$name .= " (Berlin)"; # XXX what about Potsdam? etc.?
-		if ($cat !~ m{^[US]$}) {
-		    $name .= " ($cat)";
+		$name =~ s{([sS])traße}{$1tr.}; # short form preferred by Fahrinfo
+		if (inside_berlin($r->[Strassen::COORDS()]->[0])) {
+		    $name .= " (Berlin)";
+		} elsif (inside_potsdam($r->[Strassen::COORDS()]->[0])) {
+		    $name .= " (Potsdam)";
 		}
+		$name .= " [";
+		if ($cat !~ m{^[US]$}) {
+		    $name .= "$cat; ";
+		}
+		$name .= m2km($stop->{Dist}, 1) . ']';
 
-		$lb->insert('end', $name); # XXX show also distance, maybe on map?
+		$lb->insert('end', $name); # XXX show maybe on map, somehow?
 	    }
 	    $lb->selectionSet(0);
 	}
     }
 
+    for my $def ([$start_lb, \@start_stops],
+		 [$goal_lb,  \@goal_stops],
+		) {
+	my($lb, $stops) = @$def;
+	$lb->bind('<Double-1>' => sub {
+		      my($cursel) = $lb->curselection;
+		      my $coord = $stops->[$cursel]->{StreetObj}->[Strassen::COORDS()]->[0];
+		      main::mark_point(-coords => [[[ main::transpose(split /,/, $coord) ]]],
+				       -clever_center => 1,
+				      );
+		  });
+    }
+
     $b->configure(-command => sub {
 		      my($start_name) = $start_lb->get($start_lb->curselection);
 		      my($goal_name)  = $goal_lb->get($goal_lb->curselection);
-		      $_ =~ s{\s+\((Tram|Bus)\)$}{} for ($start_name, $goal_name);
+		      $_ =~ s{\s+\[.*\]$}{} for ($start_name, $goal_name);
 		      search($start_name, $goal_name);
 		  });
 }
@@ -190,6 +219,58 @@ sub start_browser {
     require WWWBrowser;
     WWWBrowser::start_browser($url);
 }
+
+# Partially taken from Strassen::Core::nearest_point, but with
+# AllReturn really working
+sub get_nearest {
+    my($s, $xy) = @_;
+    my($x,$y) = split /,/, $xy;
+    $s->make_grid(UseCache => 1, Exact => 1) unless $s->{Grid};
+    my @res;
+    {
+	my %seen;
+	my($grx,$gry) = $s->grid($x,$y);
+	for my $xx ($grx-1 .. $grx+1) {
+	    for my $yy ($gry-1 .. $gry+1) {
+		# prevent autovivify (bad for CDB_File)
+		next unless (exists $s->{Grid}{"$xx,$yy"});
+		foreach my $n (@{ $s->{Grid}{"$xx,$yy"} }) {
+		    next if $seen{$n};
+		    $seen{$n}++;
+		    my $r = $s->get($n);
+		    next if $r->[Strassen::NAME] =~ m{^\s*$}; # no name -> no use!
+		    my($px,$py) = split /,/, $r->[Strassen::COORDS]->[0];
+		    my $this_mindist = Strassen::Util::strecke([$px,$py], [$x,$y]);
+		    my $line = {StreetObj  => $r,
+				Dist       => $this_mindist,
+			       };
+		    push @res, $line;
+		}
+	    }
+	}
+    }
+    @res = sort { $a->{Dist} <=> $b->{Dist} } @res;
+    {
+	my %seen;
+	@res = grep { !$seen{$_->{StreetObj}->[Strassen::NAME()]}++ } @res;
+    }
+    @res = @res[0..$LIMIT_LB-1] if @res >= $LIMIT_LB;
+    @res;
+}
+
+sub _inside_any {
+    my($c, $city) = @_;
+    require VectorUtil;
+    if (!$city_border_points{$city}) {
+	my $s = Strassen->new($city);
+	$s->init;
+	$city_border_points{$city} = [ map { [split /,/] } @{ $s->next->[Strassen::COORDS()] } ];
+    }
+    return 1 if VectorUtil::point_in_polygon([split /,/, $c], $city_border_points{$city});
+}
+
+sub inside_berlin  { _inside_any($_[0], 'berlin') }
+sub inside_potsdam { _inside_any($_[0], 'potsdam') }
 
 1;
 
