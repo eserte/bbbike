ea17b8c2a9d2a5f883f54f62185909967be3793a
Slaven Rezic <slaven@rezic.de>
2010-05-28 22:51:54 +0200

    * added icon for BBBikePanoramioPlugin

--- miscsrc/BBBikePanoramioPlugin.pm
+++ miscsrc/BBBikePanoramioPlugin.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2008,2009 Slaven Rezic. All rights reserved.
+# Copyright (C) 2008,2009,2010 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -20,7 +20,7 @@ push @ISA, "BBBikePlugin";
 
 use strict;
 use vars qw($VERSION);
-$VERSION = 1.06;
+$VERSION = 1.07;
 
 use File::Temp qw(tempfile);
 use JSON::XS;
@@ -28,14 +28,46 @@ use LWP::UserAgent;
 
 use vars qw(@photos);
 
+use vars qw($panoramio_bbbike_icon);
+
 sub register { 
     my $pkg = __PACKAGE__;
-
+    _create_icon();
     $main::info_plugins{$pkg} =
 	{ name => "Panoramio on BBBike",
 	  callback => sub { show_mini_images(@_) },
 	  callback_3 => sub { show_panoramio_menu(@_) },
+	  ($panoramio_bbbike_icon ? (icon => $panoramio_bbbike_icon) : ()),
 	};
+}
+
+sub _create_icon {
+    if (!defined $panoramio_bbbike_icon) {
+	$panoramio_bbbike_icon = $main::top->Photo
+	    (-format => 'gif',
+	     -data => <<EOF);
+R0lGODlhEAAQAOe9AC0tLS4uLjIyMjs7Oz09PUJCQkVFRQlWkwpZlklPVU9PTwxemVNTUxdg
+mhphmhpjnFlZWR1lnR9lnB9lniJmniNmnSVonSJpoCVpn19fXyVqoFdffilsoitsoi9soWNj
+Yy1vozpxpTZ0pxB80Tt2qEB2qD53qDt4qRt+1jt5rEF6q3JyciKA2UN7qkp6pnNzcyqD3Hd3
+dy6F20+ArlGAqXl5eUuCrkqDsEqEsDiF4VCDsFKDsFKGskCH5HB+tkCI5EyH3UOK44KCgk2K
+6YWFhV+MtUyN5k2O54mJiWGSt4OJrmGSuVmQ5liS6Y2NjVmU6WaXvGKV7ZGRkXGXvZKSkmSX
+7WubvnKZvmaY7ZSUlHWZzJeXl3adwZiYmHOfwXugwpubm4Gg0X2jxXCn4KWlpaenp6mpqYWt
+8aqqqo+vy46s9pSq+qysrJCt9o6zzpSz0a+vr4+y9Jey+Jq30J240LS0tJ640Z650py80Z69
+05y87Ku+0KvC2KvD2azD2K7D2cHBwcLCwq/G2rPE+7PI3LHK27jL3crMzr7P4bvQ4bzQ37vR
+4M7OzsPQ+cTT4cLU4r/V4c3T28nS/NTU1MnW5MnY5szb59Lf6t7e3t/f39ni7dfk7OPj49zk
+7t7j/9/k/9zq7ujo6ODq7+rq6uvr6+fr/+fu9Ovt/+7x9u/x/+z09e319fH09/T09PX19fn5
++fj6/Pf7+vr7+/r7/Pv7+/j8+vv7//n8/Pz8/P39/f39/v7//v///v//////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/////////////////////////////////yH5BAEKAP8ALAAAAAAQABAAAAj+AHv10sWqEp9E
+pmadOhPHlkCBswSpeHAhAoc0ekYMSfUwlx8MUCCJouNmgphGnj49REXCyqpbhq7sytOAUpQj
+DxE5KGRJk4gvvVSd4NIkyMM5Hh7NOLCgwx9ZS3TAYGFETa8+FkDxuoOAxw47OIpMNdKmV6cK
+eGIRKiFwk4Q3T3o8nDUFhKJFjmpdutFiDJAeVF71IpWABgUbXpJoMBFGBpMeQgrQyhAI1h4X
+IVJoibRBThW5WZwAEKgERQ4fh8BI6YXlR69QBgIILLWmFIRJZJD0kjSoFyYFK9g85EQA16gB
+rh7GKNNqwIs6UgAwEghHQBc0DD4IpGWmxpZRD3sHZcpCBJDAgAA7
+EOF
+    }
 }
 
 sub show_mini_images {
