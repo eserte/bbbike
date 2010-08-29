107b9e57ab4b7a1bd5ada1488b85b2338ef9842d
Slaven Rezic <slaven@rezic.de>
2010-08-29 22:50:04 +0200

    * cul-de-sac detection   * check script now usable   * finished checks within S-Bahn-Innenring

--- miscsrc/cul-de-sac-candidates.pl
+++ miscsrc/cul-de-sac-candidates.pl
@@ -31,6 +31,19 @@ my $only_berlin;
 GetOptions("only-berlin" => \$only_berlin)
     or die "usage: $0 [-only-berlin]";
 
+# need to find border ends
+my $cr_landstrassen;
+if ($only_berlin) {
+    my $l = Strassen->new("landstrassen");
+    $cr_landstrassen = Kreuzungen->new(UseCache => 1, AllPoints => 1, Strassen => $l);
+}
+
+my $cm_s = Strassen->new("comments_misc-orig"); # culdesac records currently only in the -orig variant
+my $culdesac_s = $cm_s->grepstreets(sub { my $r = $_;
+					  $_->[Strassen::CAT] eq 'culdesac';
+				      });
+my $culdesac = $culdesac_s->get_hashref;
+
 my $s = MultiStrassen->new("strassen",
 			   ($only_berlin ? () : ("landstrassen", "landstrassen2")),
 			   "fragezeichen",
@@ -40,8 +53,9 @@ $net->make_net_cat(UseCache => 1);
 my $cr = Kreuzungen->new(UseCache => 1, AllPoints => 1, Strassen => $s);
 while(my($k1,$v1) = each %{ $net->{Net} }) {
     if (keys %$v1 == 1) {
+	next if exists $culdesac->{$k1};
 	my($cat) = values %$v1;
-	if ($cat ne '?') {
+	if ($cat ne '?' && (!$cr_landstrassen || !$cr_landstrassen->crossing_exists($k1))) {
 	    print join("/", @{ $cr->get($k1) }), "\tX $k1\n";
 	}
     }
