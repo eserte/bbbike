985a4134e28adefc2bba9591eeb13d8a587491ab
Slaven Rezic <slaven@rezic.de>
2011-07-02 15:00:22 +0200

    * relevant_errorlog: skip some session stuff, comments

--- miscsrc/relevant_errorlog.pl
+++ miscsrc/relevant_errorlog.pl
@@ -19,11 +19,18 @@ my $file = shift || "cat /var/log/apache2/bbbike.de_error.log-???????? /var/log/
 
 open my($fh), $file;
 while(<$fh>) {
+    ## common apache errors
     m{File does not exist: } and next;
     m{attempt to invoke directory as script: } and next;
     m{script not found or unable to stat: } and next;
+    ## strip boring stuff
     s{ \[error\] }{ };
     s{ \[client 127\.0\.0\.1\] }{ };
+    ## bbbike-specific skips
+    # session expired
+    m{bbbike.cgi: Cannot draw image because session is expired} and next;
+    m{A:S:Counted: Could not open file .* for reading: No such file or directory} and next;
+    # filesystem slowness (?)
     if (m{bbbike-snapshot-debug\.cgi: .*?(\S+)$}) {
 	my $req_duration = $1;
 	if ($req_duration < 30) { # ca. 35s is the perlbal limit, which is quite critical
