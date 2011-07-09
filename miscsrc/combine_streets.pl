7b8da63a8a2d30a51f592533af4c28daeb126e67
Wolfram Schneider <wosch@freebsd.org>
2011-07-09 11:51:08 +0200

    allow to set map type in file header

--- miscsrc/combine_streets.pl
+++ miscsrc/combine_streets.pl
@@ -46,9 +46,11 @@ use Getopt::Long;
 my $make_closed_polygon;
 my $combine_same_streets;
 my $encoding;
+my $map;
 if (!GetOptions("closedpolygon!" => \$make_closed_polygon,
 		"samestreets!" => \$combine_same_streets,
 		"encoding=s" => \$encoding,
+		"map=s" => \$map,
 	       )) {
     die <<EOF;
     usage: $0 [-closedpolygon] [-samestreets] [-encoding encoding] bbdfile
@@ -80,7 +82,12 @@ sub make_long_streets {
     my $s = Strassen->new($strfile);
     $encoding = $s->get_global_directive('encoding') || $encoding;
     my $out = $s->make_long_streets;
-    $out->set_global_directives({ encoding => [$encoding] }) if $encoding;
+
+    my $header_args = {};
+    $header_args->{"encoding"} =  [$encoding] if $encoding;
+    $header_args->{"map"} =  [$map] if $map;
+    $out->set_global_directives( $header_args );
+
     $out->write("-");
 }
 
