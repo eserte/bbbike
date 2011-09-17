9e0c1974fc1707a265b5b1197725c330c742ff8e
Slaven Rezic <slaven@rezic.de>
2011-09-17 21:27:10 +0200

    * updated teaser_beta() (ferries are already in the standard version)

--- cgi/bbbike-teaser.pl
+++ cgi/bbbike-teaser.pl
@@ -192,7 +192,7 @@ sub teaser_beta {
 EOF
 	} else {
 	    <<EOF;
-<div class="teaser">Was gibt es in der <a href="$bbbike_url?info=1#beta" style="font-weight:normal;">nächsten Version</a> von www.bbbike.de?<br><b>NEU</b>: wahlweise Benutzung von Fähren</div>
+<div class="teaser">Was gibt es in der <a href="$bbbike_url?info=1#beta" style="font-weight:normal;">nächsten Version</a> von www.bbbike.de?<br></div>
 EOF
 	}
     } else {
