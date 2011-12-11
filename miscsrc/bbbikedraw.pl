12a6166cf33fd7655e1b29b91ebe22d1b563ddc3
Slaven Rezic <slaven@rezic.de>
2011-12-11 19:19:17 +0100

    * bbbikedraw.pl: new option -routelist
      This is adding a route list page to image types which support this
      (currently PDF, both in the PDF::Create and Cairo implementations)

--- miscsrc/bbbikedraw.pl
+++ miscsrc/bbbikedraw.pl
@@ -51,6 +51,7 @@ my $route_file;
 my $marker_point;
 my $q;
 my $debug;
+my $do_routelist;
 
 if (!GetOptions("w=i" => \$w,
 		"h=i" => \$h,
@@ -78,6 +79,7 @@ if (!GetOptions("w=i" => \$w,
 		"customplaces=s" => \$custom_places,
 		"routefile=s" => \$route_file,
 		"markerpoint=s" => \$marker_point,
+		"routelist!" => \$do_routelist,
 		"q|quiet!" => \$q,
 		"debug" => \$debug,
 	       )) {
@@ -225,7 +227,15 @@ if (defined $custom_places) {
 }
 if ($route_file) {
     $draw->draw_route;
+    if ($do_routelist) {
+	$draw->add_route_descr(
+			       -net => make_netz(),
+			       -lang => 'de',
+			      )
+	    if $draw->can("add_route_descr");
+    }
 }
+
 $draw->flush;
 close OUT;
 
