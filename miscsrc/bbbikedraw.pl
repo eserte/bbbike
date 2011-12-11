2e9d2d037c9acf86fd26513c955ca8f4c17140cb
Slaven Rezic <slaven@rezic.de>
2011-12-11 17:39:58 +0100

    * bbbikedraw.pl: set MakeNet parameter (used for strnames layer)

--- miscsrc/bbbikedraw.pl
+++ miscsrc/bbbikedraw.pl
@@ -2,14 +2,13 @@
 # -*- perl -*-
 
 #
-# $Id: bbbikedraw.pl,v 1.23 2008/08/22 19:47:45 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2001 Slaven Rezic. All rights reserved.
+# Copyright (C) 2001,2011 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
-# Mail: slaven.rezic@berlin.de
+# Mail: eserte@users.sourceforge.net
 # WWW:  http://www.rezic.de/eserte/
 #
 
@@ -190,6 +189,7 @@ my $draw = new BBBikeDraw
     Fh       => \*OUT,
     Geometry => $geometry,
     Draw     => [@drawtypes],
+    MakeNet  => \&make_netz,
     @extra_args,
     ;
 if (defined $bbox) {
@@ -201,12 +201,7 @@ if (defined $bbox) {
 } elsif (defined $route_file) {
     # bbox automatically set in pre_draw, see below
 } else {
-    $draw->set_bbox_max(new Strassen
-			(defined $scope && $scope eq 'region'
-			 ? "landstrassen"
-			 : (defined $scope && $scope eq 'wideregion'
-			    ? 'landstrassen2' : "strassen"))
-		       );
+    $draw->set_bbox_max(get_strassen());
 }
 $draw->init;
 if ($route_file) {
@@ -236,6 +231,34 @@ if (defined $dimfile) {
     }
     print DIM Data::Dumper->Dumpxs([$draw], ['draw']);
     close DIM;
+}
+
+{
+    my $s;
+    sub get_strassen {
+	return $s if $s;
+	$s = Strassen->new(
+			   (defined $scope && $scope eq 'region'
+			    ? 'landstrassen'
+			    : (defined $scope && $scope eq 'wideregion'
+			       ? 'landstrassen2'
+			       : 'strassen'
+			      )
+			   )
+			  );
+    }
+}
+
+{
+    my $net;
+    sub make_netz {
+	return $net if $net;
+	my $s = get_strassen();
+	require Strassen::StrassenNetz;
+	$net = StrassenNetz->new($s);
+	$net->make_net(UseCache => 1);
+	$net;
+    }
 }
 
 sub usage {
