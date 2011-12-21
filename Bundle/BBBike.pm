97acb831ba63046bbc1ac18b7afc918debdec93d
Slaven Rezic <slaven@rezic.de>
2011-12-21 21:51:39 +0100

    * Pango and Cairo as dependencies

--- Bundle/BBBike.pm
+++ Bundle/BBBike.pm
@@ -91,6 +91,10 @@ List::Permutor	- Für das Problem des Handlungsreisenden (langsamerer Permutor)
 
 PDF::Create 0.06	- Erzeugung der Route als PDF-Dokument
 
+Cairo	- Erzeugung der Route als PDF-Dokument, alternativer Renderer
+
+Pango	- Erzeugung der Route als PDF-Dokument, alternativer Renderer
+
 Font::Metrics::Helvetica	- Für die Reparatur der Zeichenbreitentabellen in PDF::Create
 
 BSD::Resource
@@ -179,7 +183,7 @@ Geo::Coder::Googlev3	- Geocoding über Googlemaps (API v3)
 
 Geo::Coder::Yahoo	- Geocoding über Yahoo
 
-Geo::Coder::Bing	- Geocoding über Bing
+Geo::Coder::Bing 0.10	- Geocoding über Bing
 
 Geo::Cloudmade	- Geocoding über Cloudmade
 
