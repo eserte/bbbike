c60ea57d5aebdc0dba00f9c57722315639379335
Slaven Rezic <slaven@rezic.de>
2012-04-26 19:21:23 +0200

    * BBBikeApacheSessionCountedHandler: return 404 in case session id   could not be found

--- BBBikeApacheSessionCountedHandler.pm
+++ BBBikeApacheSessionCountedHandler.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2011 Slaven Rezic. All rights reserved.
+# Copyright (C) 2011,2012 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -15,11 +15,11 @@ package BBBikeApacheSessionCountedHandler;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 use BBBikeApacheSessionCounted;
 
-use Apache2::Const qw(OK);
+use Apache2::Const qw(OK NOT_FOUND);
 use Apache2::RequestRec ();
 use Apache2::RequestIO ();
 
@@ -27,12 +27,16 @@ sub handler : method {
     my($class, $r) = @_;
     my $session_id = $r->args;
     my $sess = BBBikeApacheSessionCounted::tie_session($session_id);
-    if (!$sess) {
-	die "Cannot tie session with id $session_id";
+    if (!$sess || !(tied %$sess)) {
+	warn "Cannot tie session with id $session_id";
+	$r->status(NOT_FOUND);
+	$r->print("ERROR: Session id does not exist (anymore)\n");
+	OK;
+    } else {
+	$r->content_type('application/octet-stream');
+	$r->print((tied %$sess)->{serialized});
+	OK;
     }
-    $r->content_type('application/octet-stream');
-    $r->print((tied %$sess)->{serialized});
-    OK;
 }
 
 1;
