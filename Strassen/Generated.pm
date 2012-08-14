eserte/bbbike

commit ff43d35523578c013821c76c4064f7a1819769e5

* bugfix: route_to_name could crash with a division by zero error if   there were two same points in the route
  This happened usually on the radroute.html page, causing 500 server
  errors.

Strassen/Generated.pm
@@ -268,6 +268,8 @@ sub route_to_name_1 {
 	my($str_i, $rueckwaerts) = $self->net2name($xy1, $xy2);
 	my $entf = $self->{Net}{$xy1}{$xy2};
 
+	# May happen if two same points follow subsequently in the route.
+	next if !defined $entf || $entf == 0;
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);
@@ -304,10 +306,6 @@ sub route_to_name_1 {
 	    if (!defined $str) {
 		$str = "...";
 	    }
-	}
-	if (!defined $entf && $i+1 <= $#{$route_ref}) {
-	    $entf = Strassen::Util::strecke($route_ref->[$i],
-					    $route_ref->[$i+1]);
 	}
 	my($winkel, $richtung);
 	if ($i+1 < $#{$route_ref}) {
@@ -404,6 +402,8 @@ sub route_to_name_2 {
 	    }
 	}
 
+	# May happen if two same points follow subsequently in the route.
+	next if !defined $entf || $entf == 0;
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);
@@ -440,10 +440,6 @@ sub route_to_name_2 {
 	    if (!defined $str) {
 		$str = "...";
 	    }
-	}
-	if (!defined $entf && $i+1 <= $#{$route_ref}) {
-	    $entf = Strassen::Util::strecke($route_ref->[$i],
-					    $route_ref->[$i+1]);
 	}
 	my($winkel, $richtung);
 	if ($i+1 < $#{$route_ref}) {
@@ -486,7 +482,7 @@ sub route_to_name_2 {
 
     @strname;
 }
-# line 388 Generated_src.pm
+# line 386 Generated_src.pm
 sub reachable_1 {
     my($self, $coord) = @_;
     if (!exists $self->{Net}{$coord}) {
@@ -497,7 +493,7 @@ sub reachable_1 {
 	1;
     }
 }
-# line 388 Generated_src.pm
+# line 386 Generated_src.pm
 sub reachable_2 {
     my($self, $coord) = @_;
     if (!defined $self->{Net}[$self->{Coord2Index}{$coord}]) {

Strassen/Generated_src.pm
@@ -279,6 +279,8 @@ sub route_to_name_<%=$type%> {
 	    }
 	}
 <% } %>
+	# May happen if two same points follow subsequently in the route.
+	next if !defined $entf || $entf == 0;
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);
@@ -315,10 +317,6 @@ sub route_to_name_<%=$type%> {
 	    if (!defined $str) {
 		$str = "...";
 	    }
-	}
-	if (!defined $entf && $i+1 <= $#{$route_ref}) {
-	    $entf = Strassen::Util::strecke($route_ref->[$i],
-					    $route_ref->[$i+1]);
 	}
 	my($winkel, $richtung);
 	if ($i+1 < $#{$route_ref}) {

t/strassennetz.t
@@ -37,7 +37,7 @@ BEGIN {
     }
 }
 
-plan tests => 48;
+plan tests => 50;
 
 print "# Tests may fail if data changes\n";
 
@@ -320,6 +320,21 @@ EOF
     ok($r->nearest_node, "Found a nearest node with distance " . int(Strassen::Util::strecke_s($c2, $r->nearest_node)) . "m");
     my($r2) = $s_net->search($c1, $r->nearest_node, AsObj => 1);
     ok($r2->path, "Now found a path");
+}
+
+{
+    pass("-- A bug happening on the radroute.html page --");
+
+    # Twice the same coord in the route, use to cause a division by
+    # zero error somewhere
+    my $route = <<'EOF';
+#BBBike route
+$realcoords_ref = [[-3011,10103],[-2761,10323],[-2761,10323],[-2766,10325]];
+EOF
+    my $ret = Route::load_from_string($route);
+    my $path = $ret->{RealCoords};
+    my(@route) = eval { $s_net->route_to_name($path) };
+    is $@, '', 'No division by zero error';
 }
 
 __END__

commit 3c61d79b89a769edd0b200d534a5865f48c98862

* route_to_name: the commit ff43d35523578c013821c76c4064f7a1819769e5   broke the handling for lines which are not in the net (e.g.   inserted/moved points). Hopefully this is now corrected without   causing 500 server errors...

Strassen/Generated.pm
@@ -269,7 +269,11 @@ sub route_to_name_1 {
 	my $entf = $self->{Net}{$xy1}{$xy2};
 
 	# May happen if two same points follow subsequently in the route.
-	next if !defined $entf || $entf == 0;
+	next if defined $entf && $entf == 0;
+	# May happen for inserted or moved points which are not anymore in the net.
+	if (!defined $entf) {
+	    $entf = strecke([split /,/, $xy1], [split /,/, $xy2]);
+	}
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);
@@ -412,7 +416,11 @@ sub route_to_name_2 {
 	}
 
 	# May happen if two same points follow subsequently in the route.
-	next if !defined $entf || $entf == 0;
+	next if defined $entf && $entf == 0;
+	# May happen for inserted or moved points which are not anymore in the net.
+	if (!defined $entf) {
+	    $entf = strecke([split /,/, $xy1], [split /,/, $xy2]);
+	}
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);
@@ -492,7 +500,7 @@ sub route_to_name_2 {
 
     @strname;
 }
-# line 395 Generated_src.pm
+# line 399 Generated_src.pm
 sub reachable_1 {
     my($self, $coord) = @_;
     if (!exists $self->{Net}{$coord}) {
@@ -503,7 +511,7 @@ sub reachable_1 {
 	1;
     }
 }
-# line 395 Generated_src.pm
+# line 399 Generated_src.pm
 sub reachable_2 {
     my($self, $coord) = @_;
     if (!defined $self->{Net}[$self->{Coord2Index}{$coord}]) {

Strassen/Generated_src.pm
@@ -280,7 +280,11 @@ sub route_to_name_<%=$type%> {
 	}
 <% } %>
 	# May happen if two same points follow subsequently in the route.
-	next if !defined $entf || $entf == 0;
+	next if defined $entf && $entf == 0;
+	# May happen for inserted or moved points which are not anymore in the net.
+	if (!defined $entf) {
+	    $entf = strecke([split /,/, $xy1], [split /,/, $xy2]);
+	}
 	my $str;
 	if (!defined $str_i) {
 	    ($str_i, $rueckwaerts) = $self->nearest_street($xy1, $xy2);