152f53725a5395e170922aa5cd229b0d5a567110
Slaven Rezic <slaven@rezic.de>
2012-08-29 07:36:26 +0200

    * ups, missing semicolon

--- t/BBBikeTest.pm
+++ t/BBBikeTest.pm
@@ -625,7 +625,7 @@ sub _update_bbbike_test_data () {
     my $make = $^O =~ m{bsd}i ? "make" : is_in_path("freebsd-make") ? "freebsd-make" : "pmake";
     # -f BSDmakefile needed for old pmake (which may be found in Debian)
     my $cmd = "cd $testdir/data-test && $make -f BSDmakefile";
-    system $cmd
+    system $cmd;
     Test::More::diag("Error running '$cmd', expect test failures...") if $? != 0;
 }
 
