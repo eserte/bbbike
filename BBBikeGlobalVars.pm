ca9eb8bbe3ae7aad25d91d92d3b4d53faacf769f
Slaven Rezic <slaven@rezic.de>
2012-11-05 21:30:49 +0100

    * new config option center2c

--- BBBikeGlobalVars.pm
+++ BBBikeGlobalVars.pm
@@ -199,7 +199,7 @@ use vars qw($hs_check $plzmcmd $ampelstatus_label_text
 	    $balloon $c_balloon $leave_after $ch
 	    $map_bg $map_color $steady_mark $lowmem $slowcpu
 	    $use_contexthelp $use_logo $use_dialog
-	    $center_on_str $center_on_coord
+	    $center_on_str $center_on_coord $center_on_coord2
 	    $center_loaded_route $zoom_loaded_route
 	    $zoom_new_route $zoom_new_route_chooseort
 	    $set_mode $init_with_edittools $texteditor
