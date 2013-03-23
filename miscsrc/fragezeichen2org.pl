f8eedf8abf0498e70a331435c9345aec282a9a1e
Slaven Rezic <slaven@rezic.de>
2013-03-23 23:39:33 +0100

    * fragezeichen2org.pl: handling priority directive

--- miscsrc/fragezeichen2org.pl
+++ miscsrc/fragezeichen2org.pl
@@ -108,7 +108,15 @@ for my $file (@files) {
 			     $any_dist = $dist_m;
 			 }
 		     }
-		     my $headline = "** TODO <$date $wd> $subject";
+		     my $prio;
+		     if ($prio = $dir->{priority}) {
+			 $prio = $prio->[0];
+			 if ($prio !~ m{^#[ABC]$}) {
+			     warn "WARN: priority should be #A..#C, not '$prio', ignoring...\n";
+			     undef $prio;
+			 }
+		     }
+		     my $headline = "** TODO <$date $wd> " . ($prio ? "[$prio] " : "") . $subject;
 		     if ($dist_tag) {
 			 if (length($headline) + 1 + length($dist_tag) < ORG_MODE_HEADLINE_LENGTH) {
 			     $headline .= " " x (ORG_MODE_HEADLINE_LENGTH-length($headline)-length($dist_tag));
