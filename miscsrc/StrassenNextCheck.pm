3ee0b968934910ed713bda88a493d0d456a4affb
Slaven Rezic <slaven@rezic.de>
2013-04-09 21:04:05 +0200

    * StrassenNextCheck.pm: be strict when reading check_frequency   directive
      Only <days>d is accepted, as per bbd.pod.

--- miscsrc/StrassenNextCheck.pm
+++ miscsrc/StrassenNextCheck.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2012 Slaven Rezic. All rights reserved.
+# Copyright (C) 2012,2013 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -15,7 +15,7 @@ package StrassenNextCheck;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 use Strassen::Core;
 use vars qw(@ISA);
@@ -26,11 +26,8 @@ use Date::Calc qw(Add_Delta_Days);
 sub read_stream_nextcheck_records {
     my($self, $cb, %args) = @_;
 
-    my $check_frequency_days = delete $args{check_frequency_days_fallback} || 30;
     my $glob_dir = $self->get_global_directives;
-    if ($glob_dir && $glob_dir->{check_frequency}) {
-	($check_frequency_days) = $glob_dir->{check_frequency}[0] =~ m{(\d+)};
-    }
+    my $check_frequency_days = _get_check_frequency_days($glob_dir) || $args{check_frequency_days_fallback} || 30;
 
     my $passthru_without_nextcheck = delete $args{passthru_without_nextcheck};
 
@@ -74,10 +71,7 @@ sub process_nextcheck_record {
 	if (!$y) {
 	    warn "*** WARN: Malformed last_checked directive '$dir->{next_check}[0]' in '" . $self->file . "', ignoring...\n";
 	} else {
-	    my $check_frequency_days = $args{check_frequency_days};
-	    if (exists $dir->{check_frequency}) {
-		($check_frequency_days) = $dir->{check_frequency}[0] =~ m{(\d+)}; # XXX duplicated, see above
-	    }
+	    my $check_frequency_days = _get_check_frequency_days($dir) || $args{check_frequency_days};
 	    my $last_checked_date = sprintf "%04d-%02d-%02d", $y,$m,$d;
 	    ($y,$m,$d) = Add_Delta_Days($y,$m,$d, $check_frequency_days);
 	    $label = "last checked: $last_checked_date";
@@ -93,6 +87,19 @@ sub process_nextcheck_record {
 	my $date = sprintf "%04d-%02d-%02d", $y,$m,$d;
 	$dir->{_nextcheck_date}[0] = $date;
     }
+}
+
+sub _get_check_frequency_days {
+    my $dir = shift;
+    if ($dir && $dir->{check_frequency}) {
+	my($check_frequency_days) = $dir->{check_frequency}[0] =~ m{^(\d+)d$};
+	if (!$check_frequency_days) {
+	    die "ERROR: Invalid specification for check_frequency: '$dir->{check_frequency}[0]', should be <days>d.\n";
+	}
+	$check_frequency_days;
+    } else {
+	undef;
+    }    
 }
 
 1;
