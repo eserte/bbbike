68aed18e7825fd0da218761eaaeb06d200197dba
Slaven Rezic <slaven@rezic.de>
2013-05-09 21:17:17 +0200

    * require WWWBrowser was missing

--- plugins/AltBerlin.pm
+++ plugins/AltBerlin.pm
@@ -143,6 +143,7 @@ sub show_all_urls_menu {
 sub start_browser {
     my($url) = @_;
     main::status_message("Der WWW-Browser wird mit der URL $url gestartet.", "info");
+    require WWWBrowser;
     WWWBrowser::start_browser($url);
 }
 
