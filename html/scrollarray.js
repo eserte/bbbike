ca5418746da2736283ca607de6dcd2b5f6e6f2ab
Slaven Rezic <slaven@rezic.de>
2013-08-28 16:29:16 +0200

    added scrollarray.js

---
+++ html/scrollarray.js
@@ -0,0 +1,41 @@
+// (c) 2013 Slaven Rezic
+
+function ScrollArray(capacity) {
+    this.capacity = capacity;
+    this.container = [];
+    this.pos = -1;
+    this.wrapped = false;
+}
+ScrollArray.prototype.push = function(val) {
+    this.pos++;
+    if (this.pos >= this.capacity) {
+	this.pos = 0;
+	this.wrapped = true;
+    }
+    this.container[this.pos] = val;
+};
+ScrollArray.prototype.get_inx = function(inx) {
+    if (!this.wrapped) {
+	if (inx <= this.pos) {
+	    return inx;
+	} else {
+	    return null;
+	}
+    } else {
+	if (inx >= this.capacity-this.pos-1) {
+	    // left of pos
+	    return inx - (this.capacity - this.pos - 1);
+	} else {
+	    // right of pos
+	    return this.pos + 1 + inx;
+	}
+    }
+};
+ScrollArray.prototype.get_val = function(inx) {
+    var inx = this.get_inx(inx);
+    if (inx != null) {
+	return this.container[inx];
+    } else {
+	return null;
+    }
+}
