4ea6f27c347210448e84869a9df1883190fe6140
Slaven Rezic <slaven@rezic.de>
2013-08-31 10:56:07 +0200

    bbbike.bat: prefer strawberry perl over activeperl, updated comments

--- bbbike.bat
+++ bbbike.bat
@@ -1,11 +1,16 @@
 @echo off
-rem Use this if BBBike is already installed on the system using the InnoSetup installer; which means that perl 5.6.1 is available
-rem Prefer SiePerl, check for one of the both possible locations (pre-Vista,localized german and Vista)
+rem Use this batch script if BBBike is already installed on the system
+rem using the InnoSetup installer; which means that a suitable perl with
+rem required modules, especially Tk, already exists
+rem If BBBike is not installed, then fallback to either Strawberry Perl
+rem in its standard install location (Tk may be compiled with strawberry)
+rem or ActivePerl (least preferred, as Tk is not anymore availebl for
+rem ActivePerl)
 
 IF EXIST "c:\program files\bbbike\perl\bin\perl.exe" GOTO BBBikeDistEN
 IF EXIST c:\programme\bbbike\perl\bin\perl.exe GOTO BBBikeDistDE
-IF EXIST c:\perl\bin\perl.exe GOTO Activeperl
 IF EXIST c:\strawberry\perl\bin\perl.exe GOTO Strawberry
+IF EXIST c:\perl\bin\perl.exe GOTO Activeperl
 echo Cannot find any perl, fall through...
 GOTO BBBikeDistEN
 
@@ -17,12 +22,12 @@ GOTO End
 "c:\program files\bbbike\perl\bin\perl" bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO End
 
-:Activeperl
-C:\perl\bin\perl bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO End
-
 :Strawberry
 C:\strawberry\perl\bin\perl bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO End
+
+:Activeperl
+C:\perl\bin\perl bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO End
 
 :End
