9caf0e47af3437e423c210d75a28e145fa21fef1
Slaven Rezic <slaven@rezic.de>
2013-09-12 10:50:10 +0200

    missing_streets helper: use get_street_type()
    By using PLZ::get_street_type it's now possible to distinguish streets
    and non-streets, so the huge exception list could be commented out.

--- miscsrc/missing_streets.pl
+++ miscsrc/missing_streets.pl
@@ -2,10 +2,9 @@
 # -*- perl -*-
 
 #
-# $Id: missing_streets.pl,v 1.6 2009/01/18 21:53:15 eserte Exp eserte $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2008 Slaven Rezic. All rights reserved.
+# Copyright (C) 2008,2013 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -68,93 +67,94 @@ foreach my $rec (@{ $plz->{Data} }) {
     my($str, $bezirk) = ($rec->[PLZ::FILE_NAME],
 			 $rec->[PLZ::FILE_CITYPART],
 			);
-    next if $str =~ m{^[SU]-Bhf\s}; # later XXX
-    next if $str =~ m{^Güterbahnhof\s}; # later XXX
-    next if $str =~ m{\(Gaststätte\)}; # later XXX
-    next if $str =~ m{\(Kolonie\)}; # later XXX
-    next if $str =~ m{\(Siedlung\)}; # later XXX
-    next if $str =~ m{^Kolonie\s}; # later XXX
-    next if $str =~ m{^Siedlung\s}; # later XXX
-    next if $str =~ m{^Wochenendsiedlung\s}; # later XXX
-    next if $str =~ m{^KGA\s}; # later XXX
-    next if $str =~ m{^(Modersohnbrücke
-		      |Heinrich-von-Kleist-Park # Schöneberg
-		      |Englischer[ ]Garten # Tiergarten
-		      |Humboldthafen
-		      |Schloß[ ]Bellevue
-		      |Westhafen
-		      |Eichgestell # Oberschöneweide, exists as "(...)"
-		      |Waldfriedhof[ ]Oberschöneweide # Oberschöneweide
-		      |Wasserwerk[ ]an[ ]der[ ]Wuhlheide
-		      |Abstellbahnhof # Grunewald
-		      |Hundekehle
-		      |Jagdschloß[ ]Grunewald
-		      |Lindwerder
-		      |Wasserwerk[ ]Teufelssee
-		      |Melli-Besse-Str. # Adlershof --- Ring oder Straße; Lage vollkommen unklar
-		      |Ernst-Reuter-Siedlung # Wedding --- keine Straße hier zu erkennen
-		      |Humboldthain # Wedding
-		      |Albrechts[ ]Teerofen # Wannsee
-		      |Landgut[ ]Eule # Ist das eine Straße?
-		      |Glienicker[ ]Park
-		      |Im[ ]Jagen # sieht uninteressant aus
-		      |Moorlake
-		      |Nikolskoe
-		      |Pfaueninsel
-		      |Schäferberg
-		      |Siedlung[ ]10 # Baumschulenweg
-		      |Am[ ]Sportplatz # Buch
-		      |Britzer[ ]Hafen # Britz
-		      |Am[ ]Bahnhof[ ]Grunewald[ ]Vorplatz[ ]II # Charlottenburg
-                      |Avus[ ]Innenraum
-		      |Avus[ ]Nordkurve
-		      |DRK[ ]Kliniken
-		      |Europa-Center
-		      |Rudolf-Virchow-Krankenhaus
-		      |Schleuse[ ]Charlottenburg
-		      |Schleuse[ ]Plötzensee
-		      |Schleuseninsel[ ]im[ ]Tiergarten
-		      |Sportplatz[ ]Eichkamp
-		      |Sportplatz[ ]Kühler[ ]Weg
-		      |Sportplatz[ ]Maikäferpfad
-		      |Volkspark[ ]Jungfernheide
-		      |Waldbühne
-		      |Löwe-Siedlung
-		      |Jagen[ ]59 # Grünau
-		      |Waldfriedhof[ ]Grünau
-		      |AEG[ ]Siedlung[ ]I,[ ]II # Lübars
-		      |Karpfenteich-Wald
-		      |Stadtrandsiedlung # Mariendfelde
-		      |Anglerweg # Schmöckwitz (auf der RV-Karte als "Schmöckwitzwerder Süd (Anglerweg)" gekennzeichnet
-		      |Schmöckwitzwerder[ ]Süd # siehe Anglerweg
-		      |Deutscher[ ]Camping[ ]Club[ ]Krossinsee
-		      |Forstweg # keine eindeutige Referenzen
-		      |Jagen[ ]17[ ]-[ ]20
-		      |Jagen[ ]23
-		      |Jagen[ ]25
-		      |Jagen[ ]33
-		      |Jagen[ ]37
-		      |Schmöckwitzwerder[ ]Nord
-		      |Gutshof[ ]Glienicke # Kladow
-		      |Habichtsgrund
-		      |Habichtswald
-		      |Havelfreunde # Wochenendsiedlungen
-		      |Havelwiese # Kolonie
-		      |Hottengrund # Kaserne
-		      |Badewiese # Gatow
-		      |Breitehorn
-		      |Flugplatz[ ]Gatow
-		      |Ruprecht
-		      |Triftweg
-		      |Straße[ ]1[ ]\(Wiesengrund\) # Karlshorst
-		      |Behelfsheimsiedlung # Name der Siedlung zwischen Waldowallee und Köpenicker Allee
-		      |Am[ ]Elektrizitätswerk # nirgendwo gefunden
-		      |Am[ ]Hochwald # nirgendwo gefunden
-		      |Erholung # nicht in Karlshorst gefunden
-		      |Gartenfreunde[ ]Bahnhof[ ]Wuhlheide # nirgendwo gefunden
-		      |Kleckersdorfer[ ]Weg # nirgendwo gefunden
-		      |Stallwiesen # nirgendwo gefunden
-		      )$}x; # decide later (non-strassen, e.g. brunnels or parks) XXX
+    my $type = $plz->get_street_type($rec);
+    next if $type ne 'street';
+#    next if $str =~ m{^[SU]-Bhf\s}; # later XXX
+#    next if $str =~ m{^Güterbahnhof\s}; # later XXX
+#    next if $str =~ m{\(Gaststätte\)}; # later XXX
+#    next if $str =~ m{\(Kolonie\)}; # later XXX
+#    next if $str =~ m{\(Siedlung\)}; # later XXX
+#    next if $str =~ m{^Kolonie\s}; # later XXX
+#    next if $str =~ m{^Siedlung\s}; # later XXX
+#    next if $str =~ m{^Wochenendsiedlung\s}; # later XXX
+#    next if $str =~ m{^KGA\s}; # later XXX
+#    next if $str =~ m{^(Modersohnbrücke
+#		      |Heinrich-von-Kleist-Park # Schöneberg
+#		      |Englischer[ ]Garten # Tiergarten
+#		      |Humboldthafen
+#		      |Schloß[ ]Bellevue
+#		      |Westhafen
+#		      |Eichgestell # Oberschöneweide, exists as "(...)"
+#		      |Waldfriedhof[ ]Oberschöneweide # Oberschöneweide
+#		      |Wasserwerk[ ]an[ ]der[ ]Wuhlheide
+#		      |Abstellbahnhof # Grunewald
+#		      |Hundekehle
+#		      |Jagdschloß[ ]Grunewald
+#		      |Lindwerder
+#		      |Wasserwerk[ ]Teufelssee
+#		      |Melli-Besse-Str. # Adlershof --- Ring oder Straße; Lage vollkommen unklar
+#		      |Ernst-Reuter-Siedlung # Wedding --- keine Straße hier zu erkennen
+#		      |Humboldthain # Wedding
+#		      |Albrechts[ ]Teerofen # Wannsee
+#		      |Landgut[ ]Eule # Ist das eine Straße?
+#		      |Glienicker[ ]Park
+#		      |Im[ ]Jagen # sieht uninteressant aus
+#		      |Moorlake
+#		      |Nikolskoe
+#		      |Pfaueninsel
+#		      |Schäferberg
+#		      |Siedlung[ ]10 # Baumschulenweg
+#		      |Am[ ]Sportplatz # Buch
+#		      |Britzer[ ]Hafen # Britz
+#		      |Am[ ]Bahnhof[ ]Grunewald[ ]Vorplatz[ ]II # Charlottenburg
+#                      |Avus[ ]Innenraum
+#		      |Avus[ ]Nordkurve
+#		      |DRK[ ]Kliniken
+#		      |Europa-Center
+#		      |Rudolf-Virchow-Krankenhaus
+#		      |Schleuse[ ]Charlottenburg
+#		      |Schleuse[ ]Plötzensee
+#		      |Schleuseninsel[ ]im[ ]Tiergarten
+#		      |Sportplatz[ ]Eichkamp
+#		      |Sportplatz[ ]Kühler[ ]Weg
+#		      |Sportplatz[ ]Maikäferpfad
+#		      |Volkspark[ ]Jungfernheide
+#		      |Waldbühne
+#		      |Löwe-Siedlung
+#		      |Jagen[ ]59 # Grünau
+#		      |Waldfriedhof[ ]Grünau
+#		      |AEG[ ]Siedlung[ ]I,[ ]II # Lübars
+#		      |Karpfenteich-Wald
+#		      |Stadtrandsiedlung # Mariendfelde
+#		      |Anglerweg # Schmöckwitz (auf der RV-Karte als "Schmöckwitzwerder Süd (Anglerweg)" gekennzeichnet
+#		      |Schmöckwitzwerder[ ]Süd # siehe Anglerweg
+#		      |Deutscher[ ]Camping[ ]Club[ ]Krossinsee
+#		      |Forstweg # keine eindeutige Referenzen
+#		      |Jagen[ ]17[ ]-[ ]20
+#		      |Jagen[ ]23
+#		      |Jagen[ ]25
+#		      |Jagen[ ]33
+#		      |Jagen[ ]37
+#		      |Schmöckwitzwerder[ ]Nord
+#		      |Gutshof[ ]Glienicke # Kladow
+#		      |Habichtsgrund
+#		      |Habichtswald
+#		      |Havelwiese # Kolonie
+#		      |Hottengrund # Kaserne
+#		      |Badewiese # Gatow
+#		      |Breitehorn
+#		      |Flugplatz[ ]Gatow
+#		      |Ruprecht
+#		      |Triftweg
+#		      |Straße[ ]1[ ]\(Wiesengrund\) # Karlshorst
+#		      |Behelfsheimsiedlung # Name der Siedlung zwischen Waldowallee und Köpenicker Allee
+#		      |Am[ ]Elektrizitätswerk # nirgendwo gefunden
+#		      |Am[ ]Hochwald # nirgendwo gefunden
+#		      |Erholung # nicht in Karlshorst gefunden
+#		      |Gartenfreunde[ ]Bahnhof[ ]Wuhlheide # nirgendwo gefunden
+#		      |Kleckersdorfer[ ]Weg # nirgendwo gefunden
+#		      |Stallwiesen # nirgendwo gefunden
+#		      )$}x; # decide later (non-strassen, e.g. brunnels or parks) XXX
     if (exists $seen_street_with_bezirk{$str}->{$bezirk}) {
     } elsif (exists $seen_street{$str}) {
     } else {
