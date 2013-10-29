28a6074e2d1af75de130d57135060b244e173897
Slaven Rezic <slaven@rezic.de>
2013-10-29 15:26:00 +0100

    bbbike.org-download.pl refactoring
    Moved most of the code to new module BBBikeOrgDownload.pm.
    bbbike.org-download.pl is still backward compatible.

---
+++ miscsrc/BBBikeOrgDownload.pm
@@ -0,0 +1,134 @@
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2013 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+package BBBikeOrgDownload;
+
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+use File::Basename qw(basename);
+use File::Temp qw(tempdir);
+use LWP::UserAgent;
+
+use BBBikeDir qw(get_data_osm_directory);
+use BBBikeUtil qw(save_pwd2);
+use BBBikeVar ();
+
+use constant DEFAULT_ROOT_URL => 'http://download.bbbike.org/bbbike/data-osm';
+
+sub new {
+    my($class, %args) = @_;
+    my $agent_suffix = delete $args{agentsuffix}; # for tests only
+    my $download_dir = delete $args{downloaddir}; # for tests only
+    my $root_url     = delete $args{url} || DEFAULT_ROOT_URL;
+    my $debug        = delete $args{debug};
+    die "Unhandled arguments: " . join(" ", %args) if %args;
+
+    my $ua = LWP::UserAgent->new;
+    $ua->agent("bbbike/$BBBike::VERSION (" . __PACKAGE__ . "/$VERSION) (LWP::UserAgent/$LWP::VERSION) ($^O)" . ($agent_suffix ? $agent_suffix : ""));
+
+    bless {
+	   ua           => $ua,
+	   download_dir => $download_dir,
+	   root_url     => $root_url,
+	   debug        => 0,
+	  }, $class;
+}
+
+sub listing {
+    my($self) = @_;
+    my $url = $self->{root_url} . '/';
+
+    require XML::LibXML;
+    my $p = XML::LibXML->new;
+
+    my $ua = $self->{ua};
+    my $resp = $ua->get($url);
+    die "Can't get $url: " . $resp->status_line
+	if !$resp->is_success;
+
+    my $root = $p->parse_html_string($resp->decoded_content)->documentElement;
+
+    my @cities;
+    for my $a_node ($root->findnodes('//a')) {
+	my $href = $a_node->getAttribute('href');
+	next if ($href !~ m{\.tbz$});
+	$href =~ s{\.tbz$}{};
+	push @cities, $href;
+    }
+
+    @cities;
+}
+
+sub get_city {
+    my($self, $city) = @_;
+
+    my $root_url = $self->{root_url};
+    my $debug = $self->{debug};
+    my $ua = $self->{ua};
+
+    my $url = "$root_url/$city.tbz";
+    my $data_osm_directory = $self->{download_dir} || get_data_osm_directory(-create => 1);
+    my $tmpdir = tempdir(DIR => $data_osm_directory, CLEANUP => 1)
+	or die "Can't create temporary directory in $data_osm_directory: $!";
+    my $tmpfile = "$tmpdir/$city.tbz";
+    my $save_pwd = save_pwd2;
+    chdir $data_osm_directory
+	or die "Can't chdir to $data_osm_directory: $!";
+
+    print STDERR "Downloading data for $city...\n" if $debug;
+
+    my $resp = $ua->mirror($url, $tmpfile);
+    die "Can't get $url to $tmpfile: " . $resp->status_line
+	if !$resp->is_success;
+
+    print STDERR "Extracting data to $data_osm_directory...\n" if $debug;
+    if (eval { require Archive::Tar; Archive::Tar->has_bzip2_support }) {
+	my $success = Archive::Tar->extract_archive($tmpfile);
+	if (!$success) {
+	    die "Error while extracting $tmpfile with Archive::Tar: " . Archive::Tar->error;
+	}
+    } else {
+	my @cmd = ("tar", "xfj", $tmpfile);
+	system @cmd;
+	die "Error while extracting using @cmd" if $? != 0;
+    }
+
+    print STDERR "Finished.\n" if $debug;
+}
+
+1;
+
+__END__
+
+=for org TODO
+
+* Download location
+  Where exactly to download? Maybe: if bbbike is uninstalled, then
+  under .../bbbike/data-osm. If bbbike is installed, then in a user
+  directory, e.g. ~/.bbbike/data-osm.
+  -> need a function which determines the install type.
+     Does something like this exist already?
+* How bbbike-chooser.pl and bbbike.org-download.pl should interact?
+  Probably this should be a proper module. Name? BBBikeOrgDownload? Or
+  BBBikeOrg::Download?
+* tar bzip2 under Windows
+  Probably done with a module. Is it already in Strawberry?
+  -> Archive::Tar & ...::Bzip is available, just try it out
+* LWP UserAgent operation: mask as a BBBike UA
+  Find my standard UA. Don't forget version, git id etc.
+  (BBBikeHeavy::get_user_agent is problematic, because it expects to be run within bbbike, see above)
+* Updating
+  Do I need to care about things? Or just overwrite existing stuff?
+
--- miscsrc/bbbike.org_download.pl
+++ miscsrc/bbbike.org_download.pl
@@ -4,7 +4,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2011,2012 Slaven Rezic. All rights reserved.
+# Copyright (C) 2011,2012,2013 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -20,26 +20,22 @@ use lib (
 	 $FindBin::RealBin,
 	);
 
-use File::Basename qw(basename);
-use File::Temp qw(tempdir);
 use Getopt::Long;
-use LWP::UserAgent;
 
-use BBBikeDir qw(get_data_osm_directory);
-use BBBikeVar ();
+use BBBikeOrgDownload;
 
 sub usage ();
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
-my $rooturl = 'http://download.bbbike.org/bbbike/data-osm';
+my $url;
 my $city;
 my $o;
 my $agent_suffix;
 my $debug = 0;
 
 GetOptions(
-	   "url=s" => \$rooturl,
+	   "url=s" => \$url,
 	   "debug!" => \$debug,
 	   "city=s" => \$city,
 	   "o=s" => \$o, # download directory, for tests only
@@ -48,64 +44,18 @@ GetOptions(
     or usage;
 @ARGV and usage;
 
-my $ua = LWP::UserAgent->new;
-$ua->agent("bbbike/$BBBike::VERSION (bbbike.org_download.pl/$VERSION) (LWP::UserAgent/$LWP::VERSION) ($^O)" . ($agent_suffix ? $agent_suffix : ""));
+my $bod = BBBikeOrgDownload->new(
+				 agentsuffix => $agent_suffix,
+				 downloaddir => $o,
+				 url         => $url,
+				 debug       => $debug,
+				);
 
 if (!$city) {
-    listing();
+    my @cities = $bod->listing();
+    for (@cities) { print $_, "\n" }
 } else {
-    city($city);
-}
-
-sub listing {
-    my $url = $rooturl . '/';
-
-    require XML::LibXML;
-    my $p = XML::LibXML->new;
-
-    my $resp = $ua->get($url);
-    die "Can't get $url: " . $resp->status_line
-	if !$resp->is_success;
-
-    my $root = $p->parse_html_string($resp->decoded_content)->documentElement;
-
-    for my $a_node ($root->findnodes('//a')) {
-	my $href = $a_node->getAttribute('href');
-	next if ($href !~ m{\.tbz$});
-	$href =~ s{\.tbz$}{};
-	print $href, "\n";
-    }
-}
-
-sub city {
-    my $city = shift;
-    my $url = "$rooturl/$city.tbz";
-    my $data_osm_directory = $o || get_data_osm_directory(-create => 1);
-    my $tmpdir = tempdir(DIR => $data_osm_directory, CLEANUP => 1)
-	or die "Can't create temporary directory in $data_osm_directory: $!";
-    my $tmpfile = "$tmpdir/$city.tbz";
-    chdir $data_osm_directory
-	or die "Can't chdir to $data_osm_directory: $!";
-
-    print STDERR "Downloading data for $city...\n" if $debug;
-
-    my $resp = $ua->mirror($url, $tmpfile);
-    die "Can't get $url to $tmpfile: " . $resp->status_line
-	if !$resp->is_success;
-
-    print STDERR "Extracting data to $data_osm_directory...\n" if $debug;
-    if (eval { require Archive::Tar; Archive::Tar->has_bzip2_support }) {
-	my $success = Archive::Tar->extract_archive($tmpfile);
-	if (!$success) {
-	    die "Error while extracting $tmpfile with Archive::Tar: " . Archive::Tar->error;
-	}
-    } else {
-	my @cmd = ("tar", "xfj", $tmpfile);
-	system @cmd;
-	die "Error while extracting using @cmd" if $? != 0;
-    }
-
-    print STDERR "Finished.\n" if $debug;
+    $bod->get_city($city);
 }
 
 sub usage () {
@@ -115,24 +65,3 @@ EOF
 }
 
 __END__
-
-=for org TODO
-
-* Download location
-  Where exactly to download? Maybe: if bbbike is uninstalled, then
-  under .../bbbike/data-osm. If bbbike is installed, then in a user
-  directory, e.g. ~/.bbbike/data-osm.
-  -> need a function which determines the install type.
-     Does something like this exist already?
-* How bbbike-chooser.pl and bbbike.org-download.pl should interact?
-  Probably this should be a proper module. Name? BBBikeOrgDownload? Or
-  BBBikeOrg::Download?
-* tar bzip2 under Windows
-  Probably done with a module. Is it already in Strawberry?
-  -> Archive::Tar & ...::Bzip is available, just try it out
-* LWP UserAgent operation: mask as a BBBike UA
-  Find my standard UA. Don't forget version, git id etc.
-  (BBBikeHeavy::get_user_agent is problematic, because it expects to be run within bbbike, see above)
-* Updating
-  Do I need to care about things? Or just overwrite existing stuff?
-
