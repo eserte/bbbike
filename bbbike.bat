b9cd41ac7ed0e650c98c06bd85901893d1245609
Slaven Rezic <slaven@rezic.de>
2013-10-30 17:14:22 -0700

    bbbike.bat: support %ProgramFiles% variable

--- bbbike.bat
+++ bbbike.bat
@@ -4,15 +4,20 @@ rem using the InnoSetup installer; which means that a suitable perl with
 rem required modules, especially Tk, already exists
 rem If BBBike is not installed, then fallback to either Strawberry Perl
 rem in its standard install location (Tk may be compiled with strawberry)
-rem or ActivePerl (least preferred, as Tk is not anymore availebl for
+rem or ActivePerl (least preferred, as Tk is not anymore available for
 rem ActivePerl)
 
+IF EXIST "%ProgramFiles%\bbbike\perl\bin\perl.exe" GOTO BBBikeDistAny
 IF EXIST "c:\program files\bbbike\perl\bin\perl.exe" GOTO BBBikeDistEN
 IF EXIST c:\programme\bbbike\perl\bin\perl.exe GOTO BBBikeDistDE
 IF EXIST c:\strawberry\perl\bin\perl.exe GOTO Strawberry
 IF EXIST c:\perl\bin\perl.exe GOTO Activeperl
 echo Cannot find any perl, fall through...
 GOTO BBBikeDistEN
+
+:BBBikeDistAny
+"%ProgramFiles%\bbbike\perl\bin\perl" bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO End
 
 :BBBikeDistDE
 c:\programme\bbbike\perl\bin\perl bbbike %1 %2 %3 %4 %5 %6 %7 %8 %9
