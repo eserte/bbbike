533e93e47b3d82b14662e647c0de3a690e524062
Slaven Rezic <slaven@rezic.de>
2013-11-04 21:40:27 +0100

    search ptksh also in sitebin
    sitebin is for example used with Strawberry Perl, where it's different
    from scriptdir.

--- BBBikeAdvanced.pm
+++ BBBikeAdvanced.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 1999-2008,2012 Slaven Rezic. All rights reserved.
+# Copyright (C) 1999-2008,2012,2013 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -49,7 +49,7 @@ sub start_ptksh {
 	    return;
 	}
     }
-    my @perldirs = $Config{'scriptdir'};
+    my @perldirs = grep { defined $_ && -x $_ } ($Config{'sitebin'}, $Config{'scriptdir'});
     push @perldirs, dirname(dirname($^X)); # for the SiePerl installation
     my $perldir;
     TRY: {
