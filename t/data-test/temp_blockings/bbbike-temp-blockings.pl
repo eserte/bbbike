10cdab4648ee4046e4e769c98665a710247f42dc
Slaven Rezic <slaven@rezic.de>
2013-11-27 17:42:00 +0100

    data-test: add test streets for "double Ausweichroute" case

--- t/data-test/temp_blockings/bbbike-temp-blockings.pl
+++ t/data-test/temp_blockings/bbbike-temp-blockings.pl
@@ -10,4 +10,20 @@
 	q4::temp 11543,10015 11669,9987 11880,9874
 EOF
      },
+     { from  => undef, #
+       until => undef, #
+       text  => 'Voltairestr.: Weihnachtsmarkt',
+       type  => 'gesperrt',
+       data  => <<EOF,
+	2::temp 11209,12430 11329,12497
+EOF
+     },
+     { from  => undef, #
+       until => undef, #
+       text  => 'Stralauer Str.: Bauarbeiten',
+       type  => 'gesperrt',
+       data  => <<EOF,
+	1::temp 11221,12250 11300,12241
+EOF
+     },
     );
