88ba1520de0ec147c8bed582bb837b947e2850fb
Slaven Rezic <slaven@rezic.de>
2014-04-05 20:50:40 +0200

    remove unused $PROG_REVISION

--- BBBikeGlobalVars.pm
+++ BBBikeGlobalVars.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2003,2004,2007,2008,2013 Slaven Rezic. All rights reserved.
+# Copyright (C) 2003,2004,2007,2008,2013,2014 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -14,7 +14,7 @@
 package BBBikeGlobalVars;
 
 use vars qw($VERSION);
-$VERSION = '1.73';
+$VERSION = '1.74';
 
 package main;
 
@@ -24,7 +24,7 @@ use vars
   qw($bbbike_context $splash_screen $booting $status_message_dialog $status_message_toplevel
      $coords_ref $realcoords_ref $search_route_points_ref @realcoords
      %temp_blockings_on_route
-     $VERSION $PROG_REVISION $tmpdir %tmpfiles $progname
+     $VERSION $tmpdir %tmpfiles $progname
      $os $os_bsd $use_clipboard $verbose $advanced $devel_host $public_test
      $datadir $no_original_datadir $city $country
      $top %toplevel %processes $transient $c $sx $sy $top_dpi $top_dpmm @want_extends %autouse_func
