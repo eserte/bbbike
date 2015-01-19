439035aed33e5d94d57c029326dfadc3177d7b7e
Slaven Rezic <slaven@rezic.de>
2015-01-19 20:40:58 +0100

    cmdline tool for building static leaflet pages

---
+++ miscsrc/static_leaflethtml.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2015 Slaven Rezic. All rights reserved.
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+use strict;
+use FindBin;
+use lib (
+	 "$FindBin::RealBin/..",
+	 "$FindBin::RealBin/../lib",
+	);
+
+use Getopt::Long;
+
+use BBBikeLeaflet::Template;
+
+my $root_url = 'http://bbbike.de';
+my $geojson_file;
+my $geojsonp_url;
+GetOptions(
+	   'rooturl=s'              => \$root_url,
+	   'geojson|geojson-file=s' => \$geojson_file,
+	   'geojsonp-url=s'         => \$geojsonp_url,
+	  )
+    or die "usage: $0 [-rooturl ...] [-geojson ... | -geojsonp-url ...]\n";
+
+($geojson_file && $geojsonp_url)
+    and die "Can't use -geojson and -geojsonp-url together.\n";
+
+my $tpl = BBBikeLeaflet::Template->new(
+				       root_url     => $root_url,
+				       geojson_file => $geojson_file,
+				       geojsonp_url  => $geojsonp_url,
+				      );
+$tpl->process(\*STDOUT);
+
+__END__
