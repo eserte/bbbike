e3b49d6435f1b97765efadbadacd4fd1436242b9
Slaven Rezic <slaven@rezic.de>
2015-02-08 02:31:07 +0100

    bbbikeguiupdate.t: improvements
    Now a real update test (on data/ampeln) is done.

--- t/BBBikeGUIUpdateTest.pm
+++ t/BBBikeGUIUpdateTest.pm
@@ -13,13 +13,17 @@
 
 package BBBikeGUIUpdateTest;
 
-use Test::More qw(no_plan);
-
 use strict;
 use vars qw($VERSION);
 $VERSION = 0.01;
 
+use File::Compare qw(compare);
+use File::Copy qw(cp);
+use Test::More qw(no_plan);
+
 my($top, $c);
+my $sample_coord = $ENV{BBBIKE_TEST_SAMPLE_COORD};
+my $orig_file = $ENV{BBBIKE_TEST_ORIG_FILE};
 
 sub start_guitest {
     warn "Starting GUI test...\n";
@@ -29,9 +33,27 @@ sub start_guitest {
 
     $BBBike::BBBIKE_UPDATE_WWW = $ENV{BBBIKE_TEST_HTMLDIR};
 
+    main::plot("p", "lsa", -draw => 1);
+    $top->update;
+    pass("Ampeln plotted");
+
+    isa_ok $main::p_obj{lsa}, 'Strassen', 'lsa object exists';
+    ok !exists $main::ampeln{$sample_coord}, 'Sample coord is still removed';
+
+    ok compare($orig_file, 'data/ampeln') != 0, 'file content is different';
+
+    cp $orig_file, 'data/ampeln'
+	or return stop_test("Copy failed: $!");
+
+    ok !$main::p_obj{lsa}->is_current, 'lsa is not current';
+
     require Update;
     Update::bbbike_data_update();
     pass 'Update done';
+
+    ok $main::p_obj{lsa}->is_current, 'lsa is again current';
+    ok compare($orig_file, 'data/ampeln') == 0, 'file content again the same';
+    ok exists $main::ampeln{$sample_coord}, 'Sample coord is now there';
 
     exit_app();
 }
@@ -39,6 +61,12 @@ sub start_guitest {
 sub exit_app {
     main::exit_app_noninteractive();
     pass 'Application exited';
+}
+
+sub stop_test {
+    my $msg = shift;
+    main::exit_app_noninteractive();
+    fail 'Test failed: ' . $msg;
 }
 
 1;
--- t/bbbikeguiupdate.t
+++ t/bbbikeguiupdate.t
@@ -18,6 +18,9 @@ use lib (
 use ExtUtils::Manifest qw(manicopy maniread);
 use File::Copy qw(cp);
 use File::Temp qw(tempdir);
+use Tie::File ();
+
+use BBBikeUtil qw(bbbike_root);
 
 use BBBikeTest qw(check_cgi_testing $htmldir);
 check_cgi_testing;
@@ -30,6 +33,27 @@ my $t_dir = "$tmpdir/bbbike/t";
 mkdir $t_dir if !-d $t_dir;
 cp 't/BBBikeGUIUpdateTest.pm', $t_dir
     or die "Copy failed: $!";
+
+my $sample_data_file = "$tmpdir/bbbike/data/ampeln";
+
+my $sample_coord;
+chmod 0644, $sample_data_file;
+tie my @ampeln, 'Tie::File', $sample_data_file
+    or die "Can't tie $sample_data_file: $!";
+for my $i (0..$#ampeln) {
+    if ($ampeln[$i] =~ m{\tX\s+(\S+)}) {
+	$sample_coord = $1;
+	splice @ampeln, $i, 1;
+	last;
+    }
+}
+if (!$sample_coord) {
+    die "Strange: could not find a coordinate in $sample_data_file (maybe category/format changed?)\n";
+}
+$ENV{BBBIKE_TEST_SAMPLE_COORD} = $sample_coord;
+utime 0, 0, $sample_data_file
+    or die "Can't change mtime of $sample_data_file: $!";
+$ENV{BBBIKE_TEST_ORIG_FILE} = bbbike_root . '/data/ampeln';
 
 $ENV{BBBIKE_GUI_TEST_MODULE} = 'BBBikeGUIUpdateTest';
 $ENV{BBBIKE_TEST_HTMLDIR} = $htmldir;
