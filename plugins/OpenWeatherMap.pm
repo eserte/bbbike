e82e5fed21d58bd564f2d8f405d95456361fc774
Slaven Rezic <slaven@rezic.de>
2015-05-07 23:06:32 +0200

    OpenWeatherMap plugin: use api version 2.5

--- plugins/OpenWeatherMap.pm
+++ plugins/OpenWeatherMap.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2013 Slaven Rezic. All rights reserved.
+# Copyright (C) 2013,2015 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -19,7 +19,7 @@ use BBBikePlugin;
 push @ISA, 'BBBikePlugin';
 
 use strict;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use LWP::UserAgent ();
 use JSON::XS qw(decode_json);
@@ -68,31 +68,32 @@ sub add_button {
 sub refresh_owm_layer {
     eval {
 	my($minx,$miny,$maxx,$maxy) = main::get_visible_map_bbox_polar();
-	my $url = "http://api.openweathermap.org/data/2.1/find/station?bbox=$minx,$miny,$maxx,$maxy,10&cluster=yes";
+	my $cx = ($maxx-$minx)/2 + $minx;
+	my $cy = ($maxy-$miny)/2 + $miny;
+	my $url = "http://api.openweathermap.org/data/2.5/station/find?lat=$cy&lon=$cx=cnt=10";
 	my $ua = main::get_user_agent();
 	my $resp = $ua->get($url);
 	if (!$resp->is_success) {
 	    die "Fetching $url failed with " . $resp->as_string;
 	}
 	my $d = decode_json($resp->decoded_content(charset => "none"));
-	if ($d->{cod} != 200) {
-	    die "The returned JSON code was not '200', but '$d->{cod}'";
-	}
-	if (!@{ $d->{list} || [] }) {
+	if (!@$d) {
 	    die "Empty list was returned, probably no weather stations here";
 	}
 	delete_owm_layer();
-	for my $entry (@{ $d->{list} }) {
-	    my $id = $entry->{id};
+	for my $entry (@$d) {
+	    my $station = $entry->{station};
+	    my $id = $station->{id};
 	    if ($id) {
 		$CURRENT_STATIONS{$id} = $entry;
 	    }
+	    my $last_data = $entry->{last};
 	    my $text = '';
-	    $text .= 'Date/time: ' . scalar(localtime $entry->{dt}) . "\n";
-	    $text .= 'Name: ' . $entry->{name} . "\n";
-	    $text .= 'Temperature: ' . sprintf("%.1f", $entry->{main}->{temp} - 273.15) . "°C\n";
-	    $text .= 'Wind: ' . $entry->{wind}->{speed} . 'm/s, ' . $entry->{wind}->{deg} . "°\n";
-	    my($x,$y) = main::transpose($Karte::Polar::obj->map2standard($entry->{coord}->{lon}, $entry->{coord}->{lat})); # XXX other coord systems?
+	    $text .= 'Date/time: ' . scalar(localtime $last_data->{dt}) . "\n";
+	    $text .= 'Name: ' . $station->{name} . "\n";
+	    $text .= 'Temperature: ' . sprintf("%.1f", $last_data->{main}->{temp} - 273.15) . "°C\n";
+	    $text .= 'Wind: ' . $last_data->{wind}->{speed} . 'm/s, ' . $last_data->{wind}->{deg} . "°\n";
+	    my($x,$y) = main::transpose($Karte::Polar::obj->map2standard($station->{coord}->{lon}, $station->{coord}->{lat})); # XXX other coord systems?
 	    $main::c->createText($x,$y, -text => $text, -tags => ['owm-data', ($id ? "owm-data-$id" : ())]);
 	}
     };
