db7f1dd750cdc7785e45bd49d6a6d077866a93ab
Slaven Rezic <slaven@rezic.de>
2015-07-18 08:11:25 +0200

    data update

--- data/temp_blockings/bbbike-temp-blockings-optimized.pl
+++ data/temp_blockings/bbbike-temp-blockings-optimized.pl
@@ -2912,7 +2912,7 @@ userdel	2::inwork -2453,15476 -2454,15472 -2465,15471
          'data' => '#: XXX die Qualität wird nach den Bauarbeiten vermutlich besser sein vvv
 #: by: http://www.berlin.de/ba-treptow-koepenick/aktuelles/pressemitteilungen/2015/pressemitteilung.297071.php
 #: by: http://www.berlin.de/ba-treptow-koepenick/politik-und-verwaltung/aemter/strassen-und-gruenflaechenamt/gruen/artikel.295591.php
-#: last_checked: 2015-07-12 vvv
+#: last_checked: 2015-07-17 vvv
 #: check_frequency: 21d vvv
 userdel	2::inwork 14318,9688 14443,9731 14496,9705 14575,9648 14627,9609 14705,9578
 userdel	2::inwork 15052,9584 15174,9554 15321,9448 15432,9322 15513,9144 15527,9096 15638,9020
