fa5146e689c7747f630e3fa634904d8d0c9a3f78
Slaven Rezic <slaven@rezic.de>
2015-09-16 21:25:52 +0200

    bbd2transform: new operation "oneline"
    Also added Pod documentation.

--- miscsrc/bbdtransform.pl
+++ miscsrc/bbdtransform.pl
@@ -2,10 +2,9 @@
 # -*- perl -*-
 
 #
-# $Id: bbdtransform.pl,v 1.1 2004/01/10 22:52:37 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2004 Slaven Rezic. All rights reserved.
+# Copyright (C) 2004,2015 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -26,10 +25,17 @@ use Getopt::Long;
 
 my($oper, @oper_args);
 
-if (!GetOptions("translate=s" => sub {
-		    $oper = "translate";
-		    @oper_args = split /,/, $_[1];
-		})) {
+if (!GetOptions
+    (
+     'translate=s' => sub {
+	 $oper = 'translate';
+	 @oper_args = split /,/, $_[1];
+     },
+     'oneline' => sub {
+	 $oper = 'oneline';
+     },
+    ),
+   ) {
     die "usage?";
 }
 
@@ -39,6 +45,28 @@ my $s = Strassen->new($file);
 {
     no strict 'refs';
     &$oper(@oper_args);
+}
+
+sub oneline {
+    my($new_name, $new_cat, @new_coords);
+    iterate {
+	my $r = $_;
+	if (!defined $new_name) {
+	    $new_name = $r->[Strassen::NAME];
+	}
+	if (!defined $new_cat) {
+	    $new_cat = $r->[Strassen::CAT];
+	}
+	my @c = @{ $r->[Strassen::COORDS] };
+	if (@new_coords && $new_coords[-1] eq $c[0]) {
+	    shift @c;
+	}
+	push @new_coords, @c;
+    } $s;
+
+    my $new_s = Strassen->new;
+    $new_s->push([$new_name, \@new_coords, $new_cat]);
+    $new_s->write('-');
 }
 
 sub translate {
@@ -59,3 +87,39 @@ sub translate {
 }
 
 __END__
+
+=head1 NAME
+
+bbdtransform.pl - various transformations on bbd files
+
+=head1 SYNOPSIS
+
+    ./bbdtransform.pl --translate=dx,dy [file]
+
+    ./bbdtransform.pl --oneline [file]
+
+=head1 DESCRIPTION
+
+Following transformations are available:
+
+=over
+
+=item translate
+
+Translate all coordinates by the given x and y values.
+
+=item oneline
+
+Combine all lines in the bbd file to a single line. Name and category
+are taken from the first line.
+
+=back
+
+Note: currently these transformations lose all global and local
+directives.
+
+=head1 AUTHOR
+
+Slaven Rezic
+
+=cut
