d3ac6c21ea41e1fb0fc88cd2d95aa18df811c644
Slaven Rezic <slaven@rezic.de>
2016-04-09 11:13:45 +0200

    deploy-on-live.pl improvements
    --init switch
    new checked_chdir and get_current_branch functions
    Updated initialization instructions.

--- projects/git-deployment/deploy-on-live.pl
+++ projects/git-deployment/deploy-on-live.pl
@@ -4,7 +4,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2014 Slaven Rezic. All rights reserved.
+# Copyright (C) 2014,2016 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -25,9 +25,10 @@ use Term::ANSIColor 'colored';
 
 sub sudo (@);
 
-sub error ($) {
-    die colored($_[0], 'white on_red'), "\n";
-}
+sub error        ($) {  die colored($_[0], 'white on_red'), "\n" }
+sub error_no_die ($) { warn colored($_[0], 'white on_red'), "\n" }
+
+sub checked_chdir ($) { chdir $_[0] or error "Can't chdir to $_[0]: $!" }
 
 my $root_deploy_dir = '/root/work';
 my $live_symlink    = 'bbbike-webserver';
@@ -41,6 +42,7 @@ my $live_host     = 'localhost';
 
 my $dry_run;
 my $do_switch = 1;
+my $do_init;
 my $test_jobs;
 my $skip_tests;
 
@@ -50,6 +52,7 @@ GetOptions(
 	   'test-jobs=i'       => \$test_jobs,
 	   'switch!'           => \$do_switch,
 	   'skip-tests'        => \$skip_tests,
+	   'init'              => \$do_init,
 	  )
     or error "usage: $0 [--root-deploy-dir /path/to/dir] [--dry-run] [--test-jobs ...] [--skip-tests] [--no-switch]";
 
@@ -64,10 +67,18 @@ my $staging_dir = "$root_deploy_dir/$staging_symlink";
 my $red_dir     = "$root_deploy_dir/$red_basedir";
 my $blue_dir    = "$root_deploy_dir/$blue_basedir";
 
-error "The 'red' directory '$red_dir' does not exist or is not a directory" if !-d $red_dir;
-error "The 'blue' directory '$blue_dir' does not exist or is not a directory" if !-d $blue_dir;
-error "Not a symlink: '$live_dir'" if !-l $live_dir;
-error "Not a symlink: '$staging_dir'" if !-l $staging_dir;
+if ($do_init) {
+    if ($dry_run) {
+	warn "NOTE: would run a series of initialization steps...\n";
+    } else {
+	init();
+    }
+}
+
+error "The 'red' directory '$red_dir' does not exist or is not a directory, please re-run with --init" if !-d $red_dir;
+error "The 'blue' directory '$blue_dir' does not exist or is not a directory, please re-run with --init" if !-d $blue_dir;
+error "Not a symlink: '$live_dir', please re-run with --init" if !-l $live_dir;
+error "Not a symlink: '$staging_dir', please re-run with --init" if !-l $staging_dir;
 
 my $current_live_dir    = File::Spec->rel2abs(readlink($live_dir), dirname($live_dir));
 my $current_staging_dir = File::Spec->rel2abs(readlink($staging_dir), dirname($staging_dir));
@@ -80,27 +91,18 @@ error "Symlink of '$staging_dir' points to '$current_staging_dir', which is neit
 error "Live and staging symlinks point to the same directory '$current_live_dir'"
     if $current_live_dir eq $current_staging_dir;
 
-chdir "$current_live_dir/BBBike"
-    or error "Can't chdir to $current_live_dir/BBBike: $!";
+checked_chdir "$current_live_dir/BBBike";
 if (!run ["git", "status"]) {
     error "git-status in live directory failed, directory not clean?";
 }
 
-chdir "$current_staging_dir/BBBike"
-    or error "Can't chdir to $current_staging_dir/BBBike: $!";
+checked_chdir "$current_staging_dir/BBBike";
 if (!run ["git", "status"]) {
     error "git-status in staging directory failed, directory not clean?";
 }
 
 {
-    my $branches;
-    if (!run ['git', 'branch', '--quiet', '--color=never', '--contains=HEAD'], ">", \$branches) {
-	error "running git-branch failed";
-    }
-    my($current_branch) = $branches =~ m{^\* (.*)}m;
-    if (!defined $current_branch) {
-	error "cannot get current branches in staging directory, got '$branches' from git-branch call";
-    }
+    my $current_branch = get_current_branch();
     if ($current_branch ne $online_branch) {
 	error "staging directory is not on expected branch '$online_branch', but on '$current_branch'";
     }
@@ -126,8 +128,7 @@ if ($dry_run) {
     push @INC, "$current_staging_dir/BBBike";
     require BBBikeBuildUtil;
     my $pwd = save_pwd2();
-    chdir "data"
-	or die "Can't change to data subdirectory: $!";
+    checked_chdir "data";
     my $pmake = BBBikeBuildUtil::get_pmake();
     my @cmd = ($pmake, ($test_jobs ? "-j$test_jobs" : ()), "live-deployment-targets");
     system @cmd;
@@ -169,8 +170,7 @@ if ($dry_run) {
 }
 
 if ($do_switch) {
-    chdir $root_deploy_dir
-	or error "Can't chdir to $root_deploy_dir: $!";
+    checked_chdir $root_deploy_dir;
     sudo 'ln', '-snf', basename($current_staging_dir), $live_symlink
 	or error 'Symlinking the live directory failed';
     if ($current_staging_dir eq $blue_dir) {
@@ -181,8 +181,7 @@ if ($do_switch) {
 	    or warn "Symlink the staging directory failed, please check later!\n";
     }
 
-    chdir "$live_dir/BBBike"
-	or error "chdir failed: $!";
+    checked_chdir "$live_dir/BBBike";
 
     if ($dry_run) {
 	warn "NOTE: would run quick live tests now...\n";
@@ -238,8 +237,7 @@ if ($do_switch) {
 	    }
 	}
 
-	chdir "$staging_dir/BBBike"
-	    or error "chdir failed: $!";
+	checked_chdir "$staging_dir/BBBike";
 
 	my @git_tag_delete_cmd = ('git', 'tag', '-d', $tag_prefix.'/current');
 	if ($dry_run) {
@@ -263,6 +261,69 @@ END {
 	kill $sudo_validator_pid;
 	undef $sudo_validator_pid;
     }
+}
+
+sub init {
+    if (!-d $root_deploy_dir) {
+	while() {
+	    print STDERR colored("Should the root directory be created? (y/n) ", 'white on_red');
+	    chomp(my $yn = <STDIN>);
+	    if ($yn =~ m{^n}i) {
+		error 'OK, exiting deployment';
+	    } elsif ($yn !~ m{^y}i) {
+		error_no_die 'Please answer y or n';
+	    } else {
+		last;
+	    }
+	}
+	sudo 'mkdir', '-p', $root_deploy_dir;
+    }
+    if (!-d $red_dir)     { sudo 'mkdir', $red_dir }
+    if (!-d $blue_dir)    { sudo 'mkdir', $blue_dir }
+    if (!-l $live_dir)    { sudo 'ln', '-snf', $red_basedir, $live_dir }
+    if (!-l $staging_dir) { sudo 'ln', '-snf', $blue_basedir, $staging_dir }
+    for ($red_dir, $blue_dir) {
+	sudo 'chgrp', 'adm', $_;
+	sudo 'chmod', 'g+rwx', $_;
+    }
+    if (!-d "$red_dir/BBBike") {
+	my $save_pwd = save_pwd2();
+	checked_chdir $red_dir;
+	run ['git', 'clone', '--depth=1', 'git://github.com/eserte/bbbike', 'BBBike']
+	    or die "git-clone failed";
+    }
+    if (!-d "$blue_dir/BBBike") {
+	my $save_pwd = save_pwd2();
+	checked_chdir $blue_dir;
+	run ['git', 'clone', '--depth=1', 'git://github.com/eserte/bbbike', 'BBBike']
+	    or die "git-clone failed";
+    }
+    for my $dir ($red_dir, $blue_dir) {
+	my $save_pwd = save_pwd2();
+	checked_chdir "$dir/BBBike";
+	my $current_branch = get_current_branch();
+	if ($current_branch ne 'online') {
+	    if ($current_branch eq 'master') {
+		run ['git', 'checkout', '-b', 'online']
+		    or die "Cannot create+switch to online branch in $dir/BBBike";
+	    } else {
+		error "Not on expected branches master or online, but '$current_branch' (in $dir/BBBike)";
+	    }
+	}
+    }
+}
+
+sub get_current_branch {
+    my @cmd = ('git', 'branch', '--quiet', '--color=never', '--contains=HEAD');
+    my $branches;
+    if (!run [@cmd], ">", \$branches) {
+	error "running @cmd failed";
+    }
+    my($current_branch) = $branches =~ m{^\* (.*)}m;
+    if (!defined $current_branch) {
+	error "cannot get current branches in directory " . getcwd . ", got '$branches' from @cmd call";
+    }
+    $current_branch;
 }
 
 sub sudo (@) {
@@ -316,13 +377,23 @@ BEGIN {
 
 __END__
 
-# XXX first run missing, initial directories and git clones have to be created manually
-# Approximately like this:
-#     sudo aptitude install libipc-run-perl freebsd-buildutils
-#     mkdir -p /root/work/bbbike-webserver-red
-#     mkdir -p /root/work/bbbike-webserver-blue
-#     (cd /root/work/bbbike-webserver-red  && git clone --depth=1 git://github.com/eserte/bbbike BBBike && cd BBBike && git checkout -b online)
-#     (cd /root/work/bbbike-webserver-blue && git clone --depth=1 git://github.com/eserte/bbbike BBBike && cd BBBike && git checkout -b online)
-#     (cd /root/work && ln -s bbbike-webserver-blue bbbike-webserver)
-#     (cd /root/work && ln -s bbbike-webserver-red bbbike-webserver-staging)
-# Maybe do some changes to the online branch (html/newstreetform* changes, ignoring t/.prove)
+# The default deployment directory is currently /root/work. But it's
+# better to use /srv/www instead:
+#
+#    deploy-on-live.pl --root-deploy-dir /srv/www
+#
+# Before the first run, make sure that the current user is in the adm group:
+#
+#   usermod -a -G adm $USER
+#
+# ... and logout/login into the shell to make this effective.
+#
+# Minimum package prerequisites to run this script are:
+#
+#     sudo aptitude install libipc-run-perl bmake
+#
+# The very first run should be done with the --init switch (the error
+# messages will tell so otherwise)
+#
+# XXX Maybe do some changes to the online branch (html/newstreetform* changes, ignoring t/.prove)
+# XXX "cd ext && make all install" is not done automatically, but probably should.
