6df8d26dab35e1bff031f1bd3aab2ff8467f0aec
Slaven Rezic <slaven@rezic.de>
2016-06-28 21:49:45 +0200

    predictable order for sbahn/rbahn

--- miscsrc/merge_overlapping_streets.pl
+++ miscsrc/merge_overlapping_streets.pl
@@ -4,7 +4,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2004,2013 Slaven Rezic. All rights reserved.
+# Copyright (C) 2004,2013,2016 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -35,7 +35,7 @@ Slaven Rezic <slaven@rezic.de>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004,2013 Slaven Rezic. All rights reserved.
+Copyright (c) 2004,2013,2016 Slaven Rezic. All rights reserved.
 This module is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
@@ -50,6 +50,7 @@ use Strassen::Core;
 use Strassen::MultiStrassen;
 use strict;
 use Getopt::Long;
+use Tie::IxHash;
 
 my $sep = ", ";
 if (!GetOptions("sep=s" => \$sep)) {
@@ -63,7 +64,15 @@ if (!@s) {
     die "Please specify street file(s)";
 }
 
-my %points;
+tie my %points, 'Tie::IxHash';
+
+sub maybe_tie {
+    my $k = shift;
+    if (!exists $points{$k}) {
+	tie %{ $points{$k} }, 'Tie::IxHash';
+    }
+    $points{$k};
+}
 
 my $s = MultiStrassen->new(@s);
 $s->init;
@@ -74,7 +83,7 @@ while(1) {
     for my $c_i (0 .. $#$cs - 1) {
 	my $c1 = $cs->[$c_i];
 	my $c2 = $cs->[$c_i+1];
-	my $old_p = $points{$c1}{$c2} || $points{$c2}{$c1};
+	my $old_p = (exists $points{$c1} && $points{$c1}{$c2}) || (exists $points{$c2} && $points{$c2}{$c1});
 	if ($old_p) {
 	    # Append new name, but ignore empty names
 	    my $new_name = $r->[Strassen::NAME];
@@ -92,10 +101,11 @@ while(1) {
 		$old_p->[Strassen::CAT] = $new_cat;
 	    }
 	} else {
-	    $points{$c1}{$c2} = [$r->[Strassen::NAME],
-				   [$c1,$c2],
-				   $r->[Strassen::CAT],
-				  ];
+	    my $p = maybe_tie($c1);
+	    $p->{$c2} = [$r->[Strassen::NAME],
+			 [$c1,$c2],
+			 $r->[Strassen::CAT],
+			];
 	}
     }
 }
