27d46b88ccc64f1e7fa5c869dd77bfed1e14711d
Slaven Rezic <slaven@rezic.de>
2016-07-17 15:07:44 +0200

    allow bbbike_tmp_cleanup.sh on cabulja

--- miscsrc/bbbike_tmp_cleanup.sh
+++ miscsrc/bbbike_tmp_cleanup.sh
@@ -22,7 +22,7 @@ then
 fi
 
 case "`hostname`" in
-    biokovo.herceg.de | biokovo.rezic.de |biokovo-amd64.herceg.de | biokovo-amd64.rezic.de | mosor | devpc01-debian | cvrsnica.herceg.de | cvrsnica.rezic.de )
+    biokovo.herceg.de | biokovo.rezic.de |biokovo-amd64.herceg.de | biokovo-amd64.rezic.de | mosor | devpc01-debian | cvrsnica.herceg.de | cvrsnica.rezic.de | cabulja )
         ;;
     *)
         echo "Should only run on biokovo, cvrsnica, or mosor"
