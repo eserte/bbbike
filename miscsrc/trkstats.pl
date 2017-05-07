97fb1e5d7b1ee800d9890497ec6dde3f146f91a4
Slaven Rezic <slaven@rezic.de>
2017-05-07 22:03:26 +0200

    new option --filter-vehicle

--- miscsrc/trkstats.pl
+++ miscsrc/trkstats.pl
@@ -30,6 +30,7 @@ my $BBBIKEDIR = realpath "$FindBin::RealBin/..";
 GetOptions(
 	   "destdir=s" => \my $destdir,
 	   "q|quiet"   => \my $quiet,
+	   "filter-vehicle=s" => \my $filter_vehicle,
 	  )
     or die "usage: $0 [--quiet] --destdir directory trkfile ...\n";
 
@@ -50,7 +51,42 @@ for my $f (@ARGV) {
 		  with_nightride => 1,
 		  missing_vehicle_fallback => 1,
 		 );
-    DumpFile $dest, $s->human_readable;
+    my $dump = $s->human_readable;
+    if ($filter_vehicle) {
+	$dump = filter_vehicle($dump);
+    }
+    DumpFile $dest, $dump;
+}
+
+sub filter_vehicle {
+    my $dump = shift;
+    my $new_dump = {};
+    if (grep { $_ eq $filter_vehicle } @{ $dump->{vehicles} || [] }) {
+	my($min_datetime, $max_datetime);
+	for my $chunk (@{ $dump->{chunk_stats} || [] }) {
+	    if ($chunk->{vehicle} eq $filter_vehicle) {
+		push @{ $new_dump->{chunk_stats} }, $chunk;
+		if (!defined $max_datetime || (defined $chunk->{max_datetime} && $chunk->{max_datetime} gt $max_datetime)) {
+		    $max_datetime = $chunk->{max_datetime};
+		}
+		if (!defined $min_datetime || (defined $chunk->{min_datetime} && $chunk->{min_datetime} lt $min_datetime)) {
+		    $min_datetime = $chunk->{min_datetime};
+		}
+	    }
+	}
+	if (exists $dump->{nightride}) {
+	    $new_dump->{nightride} = $dump->{nightride};
+	}
+	$new_dump->{per_vehicle_stats}->{bike} = $dump->{per_vehicle_stats}->{bike};
+	$new_dump->{per_vehicle_stats}->{bike}->{max_datetime} = $max_datetime;
+	$new_dump->{per_vehicle_stats}->{bike}->{min_datetime} = $min_datetime;
+	# XXX vvv should also be filtered!
+	$new_dump->{route} = $dump->{route};
+	$new_dump->{route_areas} = $dump->{route_areas};
+	# XXX ^^^
+	$new_dump->{tags} = $dump->{tags};
+    }
+    $new_dump;
 }
 
 __END__
