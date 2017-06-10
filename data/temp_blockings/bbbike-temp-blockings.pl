8d1cd2e97abb5fa08fd666aa9f18b156b3ca9478
Slaven Rezic <slaven@rezic.de>
2017-06-10 12:07:50 +0200

    data update

--- data/temp_blockings/bbbike-temp-blockings.pl
+++ data/temp_blockings/bbbike-temp-blockings.pl
@@ -23678,7 +23678,7 @@ EOF
        source_id => 'http://www.berliner-woche.de/nachrichten/bezirk-treptow-koepenick/niederschoeneweide/artikel/45985-umleitungen-durch-angrenzende-wohngebiete/',
        data  => <<EOF,
 #: source_id: IM_022377
-userdel	q4::inwork; 19328,5304 19386,5335 19588,5385 19696,5456 19814,5512 20065,5534 20348,5509 20437,5463 20781,5242
+userdel	q4::inwork; 19328,5304 19386,5335 19588,5385 19696,5456 19814,5512 20065,5534 20348,5509 20435,5460 20740,5273 20782,5257
 EOF
      },
      { from  => 1412481600, # 2014-10-05 06:00
