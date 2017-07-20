501fb92235a75e071d5ad4904076f75473e84619
Slaven Rezic <slaven@rezic.de>
2017-07-20 20:34:06 +0200

    garmin-upload-map.pl can even download stuff

--- miscsrc/garmin-upload-map.pl
+++ miscsrc/garmin-upload-map.pl
@@ -17,9 +17,22 @@ use lib ("$ENV{HOME}/src/bbbike", "$ENV{HOME}/src/bbbike/lib");
 use GPS::BBBikeGPS::MountedDevice;
 use Cwd 'realpath';
 
-my $dir = shift
-    or die "Directory?";
-$dir = realpath $dir;
+my $dir_or_url = shift
+    or die "Directory or URL?";
+my $dir;
+if ($dir_or_url =~ m{^https?://}) {
+    require File::Temp;
+    require LWP::UserAgent;
+    my $tmpdir = File::Temp::tempdir(CLEANUP => 1, TMPDIR => 1);
+    my $ua = LWP::UserAgent->new;
+    my $resp = $ua->get($dir_or_url, ':content_file' => "$tmpdir/download.zip");
+    $resp->is_success
+	or die "Fetching $dir_or_url failed: " . $ua->status_line;
+    system("cd $tmpdir && unzip download.zip");
+    $dir = realpath glob("$tmpdir/planet*");
+} else { 
+    $dir = realpath $dir;
+}
 
 my $name;
 open my $fh, "$dir/README.txt" or die $!;
@@ -49,6 +62,7 @@ GPS::BBBikeGPS::MountedDevice->maybe_mount
 	 if ($? != 0) {
 	     warn "Copyting $srcimg -> garmin/$destimg failed";
 	 }
+	 system("ls", "-al", "$dir/garmin/$destimg");
      },
      garmin_disk_type => "card"
     );
@@ -57,7 +71,8 @@ __END__
 
 =pod
 
-Copy extracted extracts from extract.bbbike.org to garmin card,
-automatically determine file name from readme file.
+Copy extracted extracts (optionally download it) from
+extract.bbbike.org to garmin card, automatically determine file name
+from readme file.
 
 =cut
