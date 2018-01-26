70a3ded92e88f8e25c7796742df48e8ae4a72509
Slaven Rezic <slaven@rezic.de>
2018-01-26 22:32:11 +0100

    data update

--- data/temp_blockings/bbbike-temp-blockings.pl
+++ data/temp_blockings/bbbike-temp-blockings.pl
@@ -26152,7 +26152,7 @@ EOF
        type  => 'gesperrt',
        data  => <<EOF,
 #: next_check_id: DBWERKSTRASSE-2017
-#: last_checked: 2018-01-24
+#: last_checked: 2018-01-26
 	2::inwork 14231,10907 14141,10931 14076,10949
 	2::inwork 14141,10931 14107,10911
 # REMOVED ---	2::inwork 14107,10911 14224,10878 14231,10907
