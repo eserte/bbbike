75f46c217f2c6691c1ee1edda242e6c70d2e100c
Slaven Rezic <slaven@rezic.de>
2018-10-29 21:31:52 +0100

    add doit.pl file itself to list of src files in _need_rebuild

--- data/doit.pl
+++ data/doit.pl
@@ -39,7 +39,7 @@ my @check_neighbour    = ($perl, "$miscsrcdir/check_neighbour");
 sub _need_rebuild ($@) {
     my($dest, @srcs) = @_;
     return 1 if !-e $dest;
-    for my $src (@srcs) {
+    for my $src (@srcs, __FILE__) {
 	if (!-e $src) {
 	    warning "$src does not exist";
 	} else {
