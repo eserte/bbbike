2bbe6de9fd5bb1e5436f9e1766d237711f2e1915
Slaven Rezic <slaven@rezic.de>
2020-02-20 22:13:30 +0100

    travis-ci: avoid HTML::Form 6.06

--- port/travis-ci/travis-functions.sh
+++ port/travis-ci/travis-functions.sh
@@ -258,6 +258,10 @@ install_perl_dependencies() {
 	# https://github.com/bluefeet/Geo-Distance/issues/15
 	cpanm --quiet --notest 'Geo::Distance~!=0.21,!=0.22'
 
+	# HTML::Form 6.06 breaks WWW::Mechanize usage (t/mapserver.t fails)
+	# https://github.com/libwww-perl/HTML-Form/issues/22
+	cpanm --quiet --notest 'HTML::Form~!=6.06'
+
 	if [ "$CPAN_INSTALLER" = "cpanm" ]
 	then
 	    cpanm --quiet --installdeps --notest .
