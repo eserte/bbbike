57de3f14c1e4317aa197217ddd1e0edd1753c12d
Slaven Rezic <slaven@rezic.de>
2020-06-23 22:01:21 +0200

    bvg_checker.pl has a new -log option
    The old log-writing shell command was broken (bvg_checks.log was
    always empty). Also, an error was not that easy to spot, so "OK" and
    failure messages are now colored. To not get ANSI sequences into the
    log file a -log option was invented and this and STDERR written
    separately.

--- miscsrc/bvg_checker.pl
+++ miscsrc/bvg_checker.pl
@@ -4,7 +4,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2018 Slaven Rezic. All rights reserved.
+# Copyright (C) 2018,2020 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -21,12 +21,34 @@ use lib $bbbike_dir, "$bbbike_dir/lib";
 use File::Glob qw(bsd_glob);
 use Getopt::Long;
 use LWP::UserAgent;
+use Term::ANSIColor qw(colored);
 use Tie::IxHash;
 
 use Strassen::Core;
 
-GetOptions("list-only" => \my $list_only)
+GetOptions(
+	   "list-only" => \my $list_only,
+	   "log=s" => \my $log,
+	  )
     or die "usage?";
+
+my $logfh;
+if ($log) {
+    open $logfh, ">", "$log~"
+	or die "Can't write to $log~: $!";
+}
+
+sub printerr ($;$) {
+    my($msg, $color) = @_;
+    if ($color) {
+	print STDERR colored([$color], $msg);
+    } else {
+	print STDERR $msg;
+    }
+    if ($logfh) {
+	print $logfh $msg;
+    }
+}
 
 my @files = (
 	     bsd_glob("$bbbike_dir/data/*-orig"),
@@ -51,32 +73,42 @@ for my $file (@files) {
 my $errors = 0;
 if (%check_urls) {
     if ($list_only) {
-	print join("\n", keys %check_urls), "\n";
+	printerr join("\n", keys %check_urls) . "\n";
     } else {
 	my $ua = LWP::UserAgent->new(keep_alive => 1);
 
 	while(my($check_url, $strname) = each %check_urls) {
-	    print STDERR "$check_url ($strname)... ";
+	    printerr "$check_url ($strname)... ";
 	    my $resp = $ua->get($check_url);
 	    if (!$resp->is_success) {
-		print STDERR "request failed " . $resp->code;
+		printerr "request failed " . $resp->code, "red on_black";
 		$errors++;
 	    } else {
 		my $content = $resp->decoded_content;
 		if ($content =~ m{Die Meldung ist nicht verf.*?gbar}) {
-		    print STDERR "note is not available anymore";
+		    printerr "note is not available anymore", "red on_black";
 		    $errors++;
 		} else {
-		    print STDERR "OK";
+		    printerr "OK", "green on_black";
 		}
 	    }
-	    print STDERR "\n";
+	    printerr "\n";
 	}
     }
 }
 
 if ($errors) {
     exit 1;
+}
+
+# Only rename in the success case. So only bvg_checks.log~ tays around
+# and might be inspected, while a failed run would also fail the next
+# time and won't get unnoticed.
+if ($logfh) {
+    close $logfh
+	or die $!;
+    rename "$log~", $log
+	or die "Error while renaming $log~ to $log: $!";
 }
 
 __END__
