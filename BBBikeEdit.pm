19536267b779bc587d49a858aea66496e32eb7ad
Slaven Rezic <slaven@rezic.de>
2021-07-09 19:23:15 +0200

    allow more characters for $source_id

--- BBBikeEdit.pm
+++ BBBikeEdit.pm
@@ -3869,7 +3869,7 @@ sub temp_blockings_editor {
 		 chomp $selection;
 		 my($action, $content, $id) = split /[\t¦]/, $selection;
 		 $real_txt->insert("end", $content);
-		 $id =~ s{[^A-Za-z0-9/_.-]}{}g;
+		 $id =~ s{[^A-Za-z0-9/_.:,-]}{}g;
 		 $source_id = $id;
 	     } else {
 		 $real_txt->insert("end", $selection);
