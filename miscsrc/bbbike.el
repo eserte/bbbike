da76ebd631694e1f96f42bf58b9657fe1a076c99
Slaven Rezic <slaven@rezic.de>
2022-02-03 22:21:49 +0100

    bbbike-grep searches also in fragezeichen_lowprio.bbd

--- miscsrc/bbbike.el
+++ miscsrc/bbbike.el
@@ -617,8 +617,9 @@
 	(error "Can't find anything to grep for"))
     (if search-key
 	(let ((bbbike-rootdir (bbbike-rootdir))
-	      (bbbike-datadir (bbbike-datadir)))
-	  (grep (concat "2>/dev/null egrep -ins " bbbike-datadir "/*-orig " bbbike-datadir "/*.coords.data " bbbike-datadir "/temp_blockings/bbbike-temp-blockings.pl " bbbike-rootdir "/t/cgi-mechanize.t " bbbike-rootdir "/t/old_comments.t " "-e '^#:[ ]*" search-key ":?[ ]*" search-val "'"))))))
+	      (bbbike-datadir (bbbike-datadir))
+	      (fragezeichen-lowprio (concat (bbbike-aux-bbddir) "/fragezeichen_lowprio.bbd")))
+	  (grep (concat "2>/dev/null egrep -ins " (if (file-exists-p fragezeichen-lowprio) (concat fragezeichen-lowprio " ")) bbbike-datadir "/*-orig " bbbike-datadir "/*.coords.data " bbbike-datadir "/temp_blockings/bbbike-temp-blockings.pl " bbbike-rootdir "/t/cgi-mechanize.t " bbbike-rootdir "/t/old_comments.t " "-e '^#:[ ]*" search-key ":?[ ]*" search-val "'"))))))
 
 (defun bbbike-grep-button (button)
   (bbbike-grep))
