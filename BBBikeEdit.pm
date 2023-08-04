8337ba163d7db52818b0d2a73fca5ebe6d20f559
Slaven Rezic <slaven@rezic.de>
2023-08-04 21:41:40 +0200

    start_editor: emacsclient variant using goto-char
    This is currently used for temp blockings files, and is much faster
    than the normal emacsclient approach.
    
    Assisted by ChatGPT 4.

--- BBBikeEdit.pm
+++ BBBikeEdit.pm
@@ -2358,7 +2358,8 @@ sub edit_temp_blockings {
     while(<$TEMP_BLOCKINGS>) {
 	if (m<^\s*\{> || m<^\s*undef,>) {
 	    if ($record == $line) {
-		start_editor($click_info->basefile, $linenumber);
+		my $pos = tell($TEMP_BLOCKINGS) - length($_) + 1; # XXX $pos is a byte position. Currently this does not matter because bbbike-temp-blockings.pl is a latin1 file, but needs rework if this will ever be converted to utf8.
+		start_editor($click_info->basefile, $linenumber, pos => $pos);
 		return;
 	    }
 	    $record++;
@@ -2369,9 +2370,13 @@ sub edit_temp_blockings {
 }
 
 sub start_editor {
-    my($file, $line) = @_;
+    my($file, $line, %options) = @_;
+    my $pos = delete $options{pos};
+    die "Unhandled options: " . join(" ", %options) if %options;
+
     require BBBikeUtil;
     my @try = ((defined $main::texteditor && $main::texteditor !~ m{^\s*$} ? $main::texteditor : ()),
+	       "emacsclient-pos",      # emacsclient variant where goto-char is used (much faster on large files)
 	       "emacsclient",          # https://www.emacswiki.org/emacs/GnuClient: as of emacs 22.1, emacsclient supports everything gnuclient used to do
 	       "gnuclient",            # part of XEmacs
 	       "emacsclient-snapshot", # https://www.emacswiki.org/emacs/EmacsSnapshotAndDebian
@@ -2381,6 +2386,8 @@ sub start_editor {
 	my @cmd;
 	if      ($try =~ m{gnuclient} && BBBikeUtil::is_in_path($try)) {
 	    @cmd = ($try, '-q', '+'.$line, $file);
+	} elsif ($try eq 'emacsclient-pos' && defined $pos && BBBikeUtil::is_in_path('emacsclient')) {
+	    @cmd = ('emacsclient', '-n', '--eval', qq{(progn (find-file "$file") (goto-char $pos) (select-frame-set-input-focus (window-frame)))});
 	} elsif ($try =~ m{emacsclient} && BBBikeUtil::is_in_path($try)) {
 	    @cmd = ($try, '-n', '+'.$line, $file);
 	} elsif ($try eq 'vi' && BBBikeUtil::is_in_path($try) && BBBikeUtil::is_in_path("xterm")) {
