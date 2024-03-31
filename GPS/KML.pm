39d52c268d616a860b53156e9f45fca890077dec
Slaven Rezic <slaven@rezic.de>
2024-03-31 13:30:45 +0200

    GPS::KML: require Strassen::KML only if needed

--- GPS/KML.pm
+++ GPS/KML.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2007,2013 Slaven Rezic. All rights reserved.
+# Copyright (C) 2007,2013,2024 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -15,18 +15,19 @@ package GPS::KML;
 
 use strict;
 use vars qw($VERSION @ISA);
-$VERSION = '1.02';
+$VERSION = '1.03';
 
 require GPS;
 push @ISA, 'GPS';
 
-use Strassen::KML;
+# do not use Strassen::KML to be quicker on Route::load(...fuzzy...) calls
 use Route::Heavy;
 
 sub magics { ('^('.$GPS::_UTF8_BOM.')?<\?xml.*<kml') }
 
 sub convert_to_route {
     my($self, $file, %args) = @_;
+    require Strassen::KML;
     my $s = Strassen::KML->new($file);
     my $route = Route->new_from_strassen($s);
     @{ $route->{Path} };
@@ -34,6 +35,7 @@ sub convert_to_route {
 
 sub convert_from_route {
     my($self, $route, %args) = @_;
+    require Strassen::KML;
     my $s = $route->as_strassen;
     my $s_kml = Strassen::KML->new($s);
     $s_kml->bbd2kml;
