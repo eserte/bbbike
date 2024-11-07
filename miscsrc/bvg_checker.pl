edf3d101029a093c3704738f1a33842985ed8d16
Slaven Rezic <slaven@rezic.de>
2024-11-07 08:01:32 +0100

    use again BVG_SITE_IS_BROKEN
    Most items vanished.

--- miscsrc/bvg_checker.pl
+++ miscsrc/bvg_checker.pl
@@ -28,7 +28,7 @@ use POSIX qw(strftime);
 
 use Strassen::Core;
 
-use constant BVG_SITE_IS_BROKEN => 0; # strftime("%Y-%m-%d", localtime) le "2024-12-01";
+use constant BVG_SITE_IS_BROKEN => strftime("%Y-%m-%d", localtime) le "2024-11-08";
 
 return 1 if caller;
 
