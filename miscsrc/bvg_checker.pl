209672972b0914e66cf75030c754d35e9a57b440
Slaven Rezic <slaven@rezic.de>
2025-02-01 15:36:54 +0100

    set BVG_SITE_IS_BROKEN for the weekend

--- miscsrc/bvg_checker.pl
+++ miscsrc/bvg_checker.pl
@@ -28,7 +28,7 @@ use POSIX qw(strftime);
 
 use Strassen::Core;
 
-use constant BVG_SITE_IS_BROKEN => 0; # strftime("%Y-%m-%d", localtime) le "2025-01-27";
+use constant BVG_SITE_IS_BROKEN => strftime("%Y-%m-%d", localtime) le '2025-02-03';
 
 return 1 if caller;
 
