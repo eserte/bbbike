22a0c80a1a3ef1e25a0fbf7c1dd1889be6f29da6
Slaven Rezic <slaven@rezic.de>
2025-02-23 18:53:20 +0100

    extend bbbike-sourceid-in-*-regexp

--- miscsrc/bbbike.el
+++ miscsrc/bbbike.el
@@ -796,8 +796,8 @@
   'face 'bbbike-button
   'help-echo "Click button to browse (cached) URL")
 
-(setq bbbike-sourceid-in-pl-regexp  (concat "^[ ]*source_id\\(\\[inactive\\]\\)?[ ]*=>[ ]*'\\(" bbbike-viz2021-regexp "\\|[0-9][0-9B]+\\|LMS[-_][^'\"]*\\|LS/[A-Z0-9/.-]*\\|AdB/[0-9-]+\\)"))
-(setq bbbike-sourceid-in-bbd-regexp (concat "^#:[ ]*source_id\\(\\[inactive\\]\\)?:?[ ]*\\(" bbbike-viz2021-regexp "\\|[0-9][0-9B]+\\|LMS[-_][^ \n]*\\|LS/[A-Z0-9/.-]*\\|AdB/[0-9-]+\\)"))
+(setq bbbike-sourceid-in-pl-regexp  (concat "^[ ]*source_id\\(\\[inactive\\]\\)?[ ]*=>[ ]*'\\(" bbbike-viz2021-regexp "\\|[0-9][0-9B]+\\|IM_[0-9]+\\|LMS[-_][^'\"]*\\|LS/[A-Z0-9/.-]*\\|AdB/[0-9-]+\\)"))
+(setq bbbike-sourceid-in-bbd-regexp (concat "^#:[ ]*source_id\\(\\[inactive\\]\\)?:?[ ]*\\(" bbbike-viz2021-regexp "\\|[0-9][0-9B]+\\|IM_[0-9]+\\|LMS[-_][^ \n]*\\|LS/[A-Z0-9/.-]*\\|AdB/[0-9-]+\\)"))
 (setq bbbike-vmz-diff-file "~/src/bbbike-viz/diffnewvmz.bbd")
 
 ;; old definition when it was possible to create deeplinks for VMZ ids
