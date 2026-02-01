4c31c68195fe2271d46a2207308d21c962706bdc
Slaven Rezic <slaven@rezic.de>
2026-02-01 17:20:22 +0100

    resend_newstreetform_data.pl: make it work with new YAML dumps

--- miscsrc/resend_newstreetform_data.pl
+++ miscsrc/resend_newstreetform_data.pl
@@ -2,50 +2,88 @@
 # -*- perl -*-
 
 #
-# $Id: resend_newstreetform_data.pl,v 1.1 2006/08/08 19:05:04 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2006 Slaven Rezic. All rights reserved.
+# Copyright (C) 2006,2026 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
-# Mail: slaven@rezic.de
-# WWW:  http://www.rezic.de/eserte/
+# WWW:  https://github.com/eserte/bbbike
 #
 
 use strict;
 use FindBin;
+use lib "$FindBin::RealBin/..";
 use LWP::UserAgent;
-use Safe;
 use Getopt::Long;
 require "$FindBin::RealBin/../BBBikeVar.pm";
 
-my $url = "http://www/~eserte/bbbike/cgi/mapserver_comment.cgi";
+my $url = "http://localhost/bbbike/cgi/mapserver_comment.cgi";
 
 my $doit;
-GetOptions("doit" => \$doit)
-    or die "usage: $0 [-doit]";
+my $format = 'yaml';
+GetOptions(
+    "doit-local" => sub { $doit = 'local' },
+    "doit-live"  => sub { $doit = 'prod' },
+    "format=s"   => \$format,
+)
+    or die "usage: $0 [-doit-local|-doit-live] [-format yaml|dd]\n";
 
-if ($doit) {
+if ($doit && $doit eq 'prod') {
     $BBBike::BBBIKE_DIRECT_WWW = $BBBike::BBBIKE_DIRECT_WWW if 0; # cease -w
     ($url = $BBBike::BBBIKE_DIRECT_WWW) =~ s{bbbike.cgi$}{mapserver_comment.cgi};
 }
 
-my $c = Safe->new;
-
-my $res = parse_fh(\*STDIN);
+my $res = $format eq 'yaml' ? parse_fh_yaml(\*STDIN) : parse_fh_dd(\*STDIN);
 my $env = $res->{env};
 my $param = $res->{param};
 
+$param->{encoding} = 'utf-8' if !defined $param->{encoding};
 $param->{__resent__} = scalar localtime;
 
-my $ua = LWP::UserAgent->new;
-my $resp = $ua->post($url, $param, %$env);
+if ($doit) {
+    my $ua = LWP::UserAgent->new;
+    my $resp = $ua->post($url, $param, %$env);
+    require Data::Dumper; print STDERR Data::Dumper->new([$resp],[qw()])->Indent(1)->Useqq(1)->Dump;
+} else {
+    print STDERR "Would send to $url (or the production counterpart)\n";
+    require Data::Dumper; print STDERR Data::Dumper->new([$param, $env],[qw()])->Indent(1)->Useqq(1)->Sortkeys(1)->Terse(1)->Dump;
+}
 
-require Data::Dumper; print STDERR "Line " . __LINE__ . ", File: " . __FILE__ . "\n" . Data::Dumper->new([$resp],[qw()])->Indent(1)->Useqq(1)->Dump; # XXX
-
-sub parse_fh {
+sub parse_fh_yaml {
     my $fh = shift;
+    my $found_yaml_marker;
+    while(<$fh>) {
+	if (/^---$/) {
+	    $found_yaml_marker = 1;
+	    last;
+	}
+    }
+    if (!$found_yaml_marker) {
+	die "Cannot find YAML start marker (---)";
+    }
+    my $yaml = do { local $/; <$fh> };
+    require BBBikeYAML;
+    my $d = BBBikeYAML::Load($yaml);
+    for my $k (keys %$d) {
+	if (ref $d->{$k} eq 'HASH') {
+	    while(my($k2,$v2) = each %{ $d->{$k} }) {
+		$d->{$k.'.'.$k2} = $v2;
+	    }
+	    delete $d->{$k};
+	}
+    }
+    +{ param => $d,
+       env   => {}, # note: ENV is also in param. Do not use the dubious ENV-to-http-header hack
+     };
+}
+
+sub parse_fh_dd {
+    my $fh = shift;
+
+    require Safe;
+    my $c = Safe->new;
+
     my $param = {};
     my $env = {};
     while(<$fh>) {
