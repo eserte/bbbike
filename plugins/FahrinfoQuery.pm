aeec2dca9b5c87708d94b3deec0487731d24d222
Slaven Rezic <slaven@rezic.de>
2026-09-18 20:52:33 +0200

    FahrinfoQuery plugin: new VBB data

--- plugins/FahrinfoQuery.pm
+++ plugins/FahrinfoQuery.pm
@@ -16,7 +16,7 @@ package FahrinfoQuery;
 
 use strict;
 use vars qw($VERSION @ISA);
-$VERSION = '2026.06';
+$VERSION = '2026.09';
 
 use BBBikePlugin;
 push @ISA, 'BBBikePlugin';
@@ -64,7 +64,7 @@ my $bbbike_root = bbbike_root;
 # configurable
 my $openvbb_download_size = '73MB';
 my $openvbb_year = 2026;
-my $openvbb_index = 2;
+my $openvbb_index = 3;
 my $openvbb_data_url = 'https://www.vbb.de/vbbgtfs';
 ######################################################################
 
